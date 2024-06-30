0757acfa795e6700093d972c2e95b96f0f2e8fc0
이민희 <mhl98@dgu.ac.kr>
2024-07-01 00:52:33 +0900

    feat: #1 jotai 설치, 예시 추가

--- src/component/MainPage.tsx
+++ src/component/MainPage.tsx
@@ -1,7 +1,16 @@
-/*NOTE - 해당 파일은 루트 router를 위한 임시 페이지입니다.
-실제 페이지 작성 후 지워주세요! */
+/*NOTE - 해당 파일은 루트 router를 위한 임시 페이지입니다. 실제 페이지 작성 후 지워주세요! */
+import { useAtom } from "jotai";
+
+import { messageAtom } from "@/store/messageAtom";
+
 function MainPage() {
-  return <div>welcome to layer 🎇</div>;
+  const [message, setMessage] = useAtom(messageAtom);
+  return (
+    <div>
+      <span>welcome to layer 🎇</span>
+      <div>{message}</div>
+    </div>
+  );
 }
 
 export default MainPage;
