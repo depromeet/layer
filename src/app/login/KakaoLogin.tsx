88bb6dd4f32f48be9da74c5366f67f8c3a34af15
sean <sean2337@naver.com>
2024-07-03 18:51:13 +0900

    chore: 코드 간결성을 위한 type 제거

--- src/app/login/KakaoLogin.tsx
+++ src/app/login/KakaoLogin.tsx
@@ -1,7 +1,7 @@
 const Login = () => {
   const REST_API_KEY: string = import.meta.env.VITE_REST_API_KEY;
   const REDIRECT_URI: string = import.meta.env.VITE_REDIRECT_URI;
-  const link: string = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+  const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   const loginHandler = () => {
     window.location.href = link;
