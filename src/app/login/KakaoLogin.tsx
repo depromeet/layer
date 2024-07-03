7c62d456ed5c5b6e2b2bb69c4864eeaa72775625
sean <sean2337@naver.com>
2024-07-03 11:28:38 +0900

    chore: #1 modify kakao social key name

--- src/app/login/KakaoLogin.tsx
+++ src/app/login/KakaoLogin.tsx
@@ -1,6 +1,6 @@
 const Login = () => {
-  const REST_API_KEY: string = import.meta.env.REST_API_KEY;
-  const REDIRECT_URI: string = import.meta.env.REDIRECT_URI;
+  const REST_API_KEY: string = import.meta.env.VITE_REST_API_KEY;
+  const REDIRECT_URI: string = import.meta.env.VITE_REDIRECT_URI;
   const link: string = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   const loginHandler = () => {
