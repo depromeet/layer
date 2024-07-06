1e00004537b0a153d36420aaf7ad3e011d3eaf07
donghunee <ehdgns1766@naver.com>
2024-07-06 16:21:56 +0900

    feat: Modal Componet 추가

---
+++ src/component/common/modal/Modal.tsx
@@ -0,0 +1,82 @@
+import ModalPortal from "@/component/common/modal/ModalPortal";
+import { css } from "@emotion/react";
+import { memo, useEffect, useRef } from "react";
+
+interface Props {
+  isModalOpen: boolean;
+  onClose: () => void;
+  children?: React.ReactNode;
+}
+
+function Modal({ onClose, isModalOpen, children }: Props) {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  // 모달 오픈 시 스크롤 방지
+  useEffect(() => {
+    document.body.style.overflow = isModalOpen ? "hidden" : "auto";
+  }, [isModalOpen]);
+
+  // 모달 오픈 시 뒤로가기 방지 (고민중)
+  useEffect(() => {
+    const preventGoBack = () => {
+      history.go(1);
+      onClose();
+    };
+
+    history.pushState(null, "", location.href);
+    window.addEventListener("popstate", preventGoBack);
+
+    return () => window.removeEventListener("popstate", preventGoBack);
+  }, [onClose]);
+
+  // 모달 영역 이외 클릭 시 모달 닫기
+  useEffect(() => {
+    const listener = (e: MouseEvent) => {
+      if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+        onClose();
+      }
+    };
+    window.addEventListener("mousedown", listener);
+    return () => {
+      window.removeEventListener("mousedown", listener);
+    };
+  }, [onClose, modalRef]);
+
+  if (!isModalOpen) return null;
+
+  return (
+    <ModalPortal>
+      <div
+        css={css`
+          position: fixed;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          width: 100vw;
+          height: 100dvh;
+          background-color: rgba(0, 0, 0, 0.4);
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        `}
+      >
+        <div
+          ref={modalRef}
+          css={css`
+            padding: 20px;
+            min-width: 300px;
+            min-height: 100px;
+            background-color: #ffffff;
+            border: 1px solid #cbcbcb;
+            border-radius: 10px;
+          `}
+        >
+          {children}
+        </div>
+      </div>
+    </ModalPortal>
+  );
+}
+
+export default memo(Modal);
