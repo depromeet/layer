956fbc80ed5db3af390a32fcf5472ae3201973d8
klmhyeonwoo <gentlemonster77@likelion.org>
2024-07-08 21:00:14 +0900

    feat: #11 Add Button Component

--- src/component/Button/ButtonProvider.tsx
+++ src/component/Button/ButtonProvider.tsx
@@ -4,31 +4,29 @@ import { Children, cloneElement, isValidElement, PropsWithChildren } from "react
 import Button, { ButtonProps } from "@/component/Button/Button.tsx";
 
 const Primary = ({ ...props }) => {
-  return <Button {...props} colorSchema={"primary"} />;
+  return <Button colorSchema={"primary"} {...props} />;
 };
 
 const Sky = ({ ...props }) => {
-  return <Button {...props} colorSchema={"sky"} />;
+  return <Button colorSchema={"sky"} {...props} />;
 };
 
 const Gray = ({ ...props }) => {
-  return <Button {...props} colorSchema={"gray"} />;
+  return <Button colorSchema={"gray"} {...props} />;
 };
 
 export const ButtonProvider = ({ children, ...props }: PropsWithChildren<ButtonProps>) => {
   return (
     <div
       css={css`
+        display: flex;
+        flex-direction: column;
         position: sticky;
         bottom: 0;
 
-        padding-bottom: 2rem;
+        padding: 4rem 0 2rem 0;
         margin-top: auto;
-
-        display: flex;
-        flex-direction: column;
         row-gap: 0.6rem;
-        flex: none;
       `}
     >
       {Children.map(children, (child) => {
