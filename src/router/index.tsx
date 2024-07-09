c643cba3a5885d8d89be787af25364ee10528bc4
sean <sean2337@naver.com>
2024-07-09 14:53:29 +0900

    feat #11: Add Router to Login Page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,6 +2,7 @@ import { Fragment } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import MainPage from "@/app/MainPage.tsx"; /* FIXME - 실제 메인 페이지 작성 후 대체해주세요. */
+import Login from "@/app/login/Login";
 import Staging from "@/app/test/Staging.tsx";
 import GlobalLayout from "@/layout/GlobalLayout.tsx";
 
@@ -13,6 +14,10 @@ const routerChildren = [
   {
     path: "/staging",
     element: <Staging />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ];
 
