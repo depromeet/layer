83ec37b6f770148ed697a4f35aeb74179d535193
이민희 <mhl98@dgu.ac.kr>
2024-07-10 02:12:41 +0900

    chore: #11 라디오버튼 사용 예시

--- src/app/test/Staging.tsx
+++ src/app/test/Staging.tsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
+
 import Button from "@/component/Button/Button.tsx";
 import { ButtonProvider } from "@/component/Button/ButtonProvider.tsx";
+import Radio from "@/component/common/RadioButton/Radio";
+import RadioButtonGroup from "@/component/common/RadioButton/RadioButtonGroup";
 import { DefaultLayout } from "@/layout/DefaultLayout.tsx";
 
 export default function Staging() {
+  const [selectedValue, setSelectedValue] = useState<string>();
   return (
     <DefaultLayout>
       <Button> 그냥 그저 그런 버튼 </Button>
       <Button colorSchema={"gray"}> 그냥 그저 그런 버튼 </Button>
       <Button colorSchema={"sky"}> 그냥 그저 그런 버튼 </Button>
       <Button colorSchema={"primary"}> 그냥 그저 그런 버튼 </Button>
+
+      <RadioButtonGroup radioName={"프로젝트 주기"} selectedValue={selectedValue} onChange={setSelectedValue}>
+        <Radio value={"0"}>주 1회</Radio>
+        <Radio value={"1"}>월 1회</Radio>
+        <Radio value={"2"}>분기별</Radio>
+        <Radio value={"3"}>프로젝트 끝난 후</Radio>
+      </RadioButtonGroup>
 
       <ButtonProvider>
         <ButtonProvider.Primary>기본 버튼</ButtonProvider.Primary>
