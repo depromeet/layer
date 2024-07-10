3c4f7d26a9dc1a26869db3eeb92844d9f68568e5
sean <sean2337@naver.com>
2024-07-10 23:32:56 +0900

    feat: Add Login Page Default Layout

--- src/app/login/Login.tsx
+++ src/app/login/Login.tsx
@@ -5,7 +5,7 @@ import { googleLogin } from "./google/googleLogin";
 
 const Login = () => {
   return (
-    <DefaultLayout appBarVisible={false}>
+    <DefaultLayout title="떡잎마을 방법대" RightComp={<>Right Comp</>}>
       <SocialLoginButton type="kakao" handler={kakaoLogin} />
       <SocialLoginButton type="google" handler={googleLogin} />
     </DefaultLayout>
