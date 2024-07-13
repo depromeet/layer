775bfc882483ea5d470476f549d4a6bb209e8a37
이민희 <mhl98@dgu.ac.kr>
2024-07-14 00:01:03 +0900

    chore: #11 불필요한 타입 선언 제거

--- src/hooks/useCheckBox.ts
+++ src/hooks/useCheckBox.ts
@@ -1,13 +1,10 @@
 import { useState } from "react";
 
-import { CheckBoxContextState } from "@/store/context/CheckBoxContext";
-
-type UseCheckBoxReturn = [CheckBoxContextState["isChecked"], CheckBoxContextState["onChange"], string[]];
-
-export const useCheckBox = (): UseCheckBoxReturn => {
+export const useCheckBox = () => {
   const [checkedStates, setCheckedStates] = useState<Record<string, boolean>>({});
   const isChecked = (value: string) => checkedStates[value];
   const toggle = (value: string) => setCheckedStates((prev) => ({ ...prev, [value]: !prev[value] }));
   const selectedValues = Object.keys(checkedStates).filter((key) => checkedStates[key]);
-  return [isChecked, toggle, selectedValues];
+
+  return [isChecked, toggle, selectedValues] as const;
 };
--- src/hooks/useRadioButton.ts
+++ src/hooks/useRadioButton.ts
@@ -1,13 +1,9 @@
 import { useState } from "react";
 
-import { RadioContextState } from "@/store/context/RadioContext";
-
-type UseRadioButtonReturn = [RadioContextState["isChecked"], RadioContextState["onChange"], string | undefined];
-
-export const useRadioButton = (): UseRadioButtonReturn => {
+export const useRadioButton = () => {
   const [selectedValue, setSelectedValue] = useState<string>();
   const isChecked = (value: string) => selectedValue === value;
   const onChange = (value: string) => setSelectedValue(value);
 
-  return [isChecked, onChange, selectedValue];
+  return [isChecked, onChange, selectedValue] as const;
 };
