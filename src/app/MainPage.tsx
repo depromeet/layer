4ebdd8915614405fa247a773008e194e3e75a2a5
donghunee <ehdgns1766@naver.com>
2024-07-15 00:42:27 +0900

    refactor: #11 Toast 예시 추가 및 convention 적용

--- src/app/MainPage.tsx
+++ src/app/MainPage.tsx
@@ -1,14 +1,18 @@
 /*NOTE - 해당 파일은 루트 router를 위한 임시 페이지입니다. 실제 페이지 작성 후 지워주세요! */
 import { useAtom } from "jotai";
 
+import Button from "@/component/Button/Button";
+import { Icon } from "@/component/common/Icon/Icon";
+import { Modal } from "@/component/common/Modal/Modal";
+import { ToastList } from "@/component/common/Toast/ToastList";
+import { useModal } from "@/hooks/useModal";
+import { useToast } from "@/hooks/useToast";
 import { messageAtom } from "@/store/messageAtom.tsx";
-import Modal from "@/component/common/modal/Modal";
-import useModal from "@/hooks/useModal";
-import Icon from "@/component/common/Icon/Icon";
 
 function MainPage() {
   const [message] = useAtom(messageAtom);
   const { open } = useModal();
+  const { toast } = useToast();
 
   return (
     <>
@@ -22,7 +26,13 @@ function MainPage() {
       <Icon icon="ic_back" color="#00ff00" size={4} />
       <Icon icon="ic_check" color="black" size={4} />
 
+      <Button onClick={() => toast.success("성공")} css={{ marginBottom: "1rem" }}>
+        Success Toast
+      </Button>
+      <Button onClick={() => toast.error("에러")}>Error Toast</Button>
+
       <Modal />
+      <ToastList />
     </>
   );
 }
