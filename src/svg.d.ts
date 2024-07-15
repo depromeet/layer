2d54ac00750d68e4ea35a6552d87769065f5548b
sean <sean2337@naver.com>
2024-07-15 21:53:15 +0900

    fix/#28/SpriteSvg error fix

--- src/svg.d.ts
+++ src/svg.d.ts
@@ -2,3 +2,8 @@ declare module "*.svg" {
   const value: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default value;
 }
+
+declare module "@/assets/svgs/spriteSvgs/loginLogo.svg" {
+  const content: string;
+  export default content;
+}
