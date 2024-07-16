2359c22a09073db07d8683042b7738cb193845a5
sean <sean2337@naver.com>
2024-07-16 22:18:00 +0900

    feat:#35 Add User Icon

--- src/assets/svgs/common/index.ts
+++ src/assets/svgs/common/index.ts
@@ -1,4 +1,5 @@
 export { default as ic_back } from "./ic_back.svg?react";
 export { default as ic_check } from "./ic_check.svg?react";
 export { default as ic_plus } from "./ic_plus.svg?react";
+export { default as ic_user } from "./ic_user.svg?react";
 export { default as basicProfile } from "./basicProfile.svg?react";
