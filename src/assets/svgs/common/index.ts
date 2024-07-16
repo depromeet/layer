50549868993c8318612b7c886823573abe940f3b
sean <sean2337@naver.com>
2024-07-16 17:25:53 +0900

    feat:#35 Home SVGs 추가

--- src/assets/svgs/common/index.ts
+++ src/assets/svgs/common/index.ts
@@ -1,3 +1,4 @@
 export { default as ic_back } from "./ic_back.svg?react";
 export { default as ic_check } from "./ic_check.svg?react";
-export { default as layer_small } from "./layer_small.svg?react";
+export { default as ic_plus } from "./ic_plus.svg?react";
+export { default as basicProfile } from "./basicProfile.svg?react";
