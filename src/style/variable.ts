de27a055e6643c1fbd94d6a5ebd65534a2a01706
sean <sean2337@naver.com>
2024-07-16 22:18:19 +0900

    feat:#35 Design Token 임시 삽입

--- src/style/variable.ts
+++ src/style/variable.ts
@@ -105,7 +105,8 @@ export const DESIGN_SYSTEM_TEXT = {
 };
 
 export const DESIGN_SYSTEM_COLOR = {
-  darkGrey: "#868e96",
+  darkGray: "#868e96",
+  darkGrayText: "#495057",
   inverseGrey50: "#202027",
   inverseGrey100: "#2c2c35",
   inverseGrey200: "#3c3c47",
