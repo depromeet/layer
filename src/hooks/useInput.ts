2bf4eeaec9cfb8e9e440fefbf9ff403732f2a9b5
이민희 <mhl98@dgu.ac.kr>
2024-07-17 22:43:22 +0900

    feat: #11 textarea 추가

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,9 +1,9 @@
 import { useCallback, useState } from "react";
 
-export const useInput = (defaultValue?: string) => {
+export const useInput = <T extends HTMLInputElement | HTMLTextAreaElement>(defaultValue?: string) => {
   const [value, setValue] = useState(defaultValue || "");
 
-  const handleInputChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = useCallback((e: React.ChangeEvent<T>) => {
     setValue(e.target.value);
   }, []);
 
