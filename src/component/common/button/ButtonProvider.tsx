ba987bb83b84ec8330e5432736705a28485d84e3
klmhyeonwoo <gentlemonster77@likelion.org>
2024-07-18 01:54:03 +0900

    feat: #11 Add Button Provider Sort

--- src/component/common/button/ButtonProvider.tsx
+++ src/component/common/button/ButtonProvider.tsx
@@ -3,6 +3,12 @@ import { Children, cloneElement, isValidElement, PropsWithChildren } from "react
 
 import { Button, ButtonProps } from "@/component/common/button/Button.tsx";
 
+type SortSet = "vertical" | "horizontal";
+
+type ButtonProviderProps = {
+  sort?: SortSet;
+} & ButtonProps;
+
 const Primary = ({ ...props }) => {
   return <Button colorSchema={"primary"} {...props} />;
 };
@@ -15,18 +21,25 @@ const Gray = ({ ...props }) => {
   return <Button colorSchema={"gray"} {...props} />;
 };
 
-export const ButtonProvider = ({ children, ...props }: PropsWithChildren<ButtonProps>) => {
+export const ButtonProvider = ({ sort = "vertical", children, ...props }: PropsWithChildren<ButtonProviderProps>) => {
   return (
     <div
       css={css`
         display: flex;
-        flex-direction: column;
+        ${sort === "horizontal"
+          ? css`
+              align-items: center;
+              column-gap: 1.7rem;
+            `
+          : css`
+              flex-direction: column;
+              row-gap: 0.6rem;
+            `}
         position: sticky;
         bottom: 0;
 
         padding: 4rem 0 2rem 0;
         margin-top: auto;
-        row-gap: 0.6rem;
       `}
     >
       {Children.map(children, (child) => {
