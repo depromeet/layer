9d75ffbb7428990b05c22f8e13e9e0d0ed22013a
이민희 <mhl98@dgu.ac.kr>
2024-07-18 23:27:29 +0900

    refactor: #35 DefaultLayout, ProgressBar 밖으로 이동

--- src/component/retrospectCreate/steps/MainInfo.tsx
+++ src/component/retrospectCreate/steps/MainInfo.tsx
@@ -4,23 +4,15 @@ import { useContext } from "react";
 import { RetrospectCreateContext } from "@/app/retrospectCreate/RetrospectCreate";
 import { ButtonProvider } from "@/component/common/button";
 import { Header } from "@/component/common/header";
-import { Icon } from "@/component/common/Icon";
 import { Input, InputLabelContainer, Label, TextArea } from "@/component/common/input";
-import { ProgressBar } from "@/component/common/ProgressBar";
 import { useInput } from "@/hooks/useInput";
-import { DefaultLayout } from "@/layout/DefaultLayout";
 
-type MainInfoProps = {
-  curPage: number;
-};
-
-export function MainInfo({ curPage }: MainInfoProps) {
-  const { goNext, goPrev, totalStepsCnt } = useContext(RetrospectCreateContext);
+export function MainInfo() {
+  const { goNext } = useContext(RetrospectCreateContext);
   const { value: retroName, handleInputChange: handleNameChange } = useInput();
   const { value: description, handleInputChange: handleDescriptionChange } = useInput();
   return (
-    <DefaultLayout LeftComp={<Icon icon={"ic_arrow_back"} onClick={goPrev} />}>
-      <ProgressBar curPage={curPage} lastPage={totalStepsCnt} />
+    <>
       <Header title={"회고의\n이름은 무엇인가요?"} />
       <div
         css={css`
@@ -46,6 +38,6 @@ export function MainInfo({ curPage }: MainInfoProps) {
           다음
         </ButtonProvider.Primary>
       </ButtonProvider>
-    </DefaultLayout>
+    </>
   );
 }
