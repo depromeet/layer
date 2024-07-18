a0004d6c47e332f845ae3146186ac866f8dde883
이민희 <mhl98@dgu.ac.kr>
2024-07-18 18:22:51 +0900

    fix: #11 tipcard에 css style props 추가

--- src/component/common/tip/TipCard.tsx
+++ src/component/common/tip/TipCard.tsx
@@ -1,23 +1,27 @@
-import { css } from "@emotion/react";
+import { css, Interpolation, Theme } from "@emotion/react";
 
 import { Typography } from "@/component/common/typography";
 import { DESIGN_SYSTEM_COLOR } from "@/style/variable";
 
 type TipCardProps = {
   message: string;
+  styles: Interpolation<Theme>;
 };
 
-export function TipCard({ message }: TipCardProps) {
+export function TipCard({ message, styles }: TipCardProps) {
   return (
     <div
-      css={css`
-        background-color: ${DESIGN_SYSTEM_COLOR.theme}10;
-        padding: 1.4rem 1.8rem;
-        border-radius: 0.8rem;
-        display: flex;
-        flex-direction: column;
-        gap: 0.4rem;
-      `}
+      css={[
+        css`
+          background-color: ${DESIGN_SYSTEM_COLOR.theme}10;
+          padding: 1.4rem 1.8rem;
+          border-radius: 0.8rem;
+          display: flex;
+          flex-direction: column;
+          gap: 0.4rem;
+        `,
+        styles,
+      ]}
     >
       {/**FIXME - typography design token */}
       <Typography color={"theme2"} variant={"CAPTION"}>
