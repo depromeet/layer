2004ac99eb4f1adf077682481e1bf5fae476fa0a
이민희 <mhl98@dgu.ac.kr>
2024-07-18 21:04:34 +0900

    fix: #35 DefaultLayout을 단계별 컴포넌트 내부로 이동

--- src/component/retrospectCreate/steps/CustomTemplate.tsx
+++ src/component/retrospectCreate/steps/CustomTemplate.tsx
@@ -1,7 +1,9 @@
 import { useContext } from "react";
 
 import { RetrospectCreateContext } from "@/app/retrospectCreate/RetrospectCreate";
+import { Icon } from "@/component/common/Icon";
 import { ProgressBar } from "@/component/common/ProgressBar";
+import { DefaultLayout } from "@/layout/DefaultLayout";
 
 type CustomTemplateProps = {
   curPage: number;
@@ -9,5 +11,9 @@ type CustomTemplateProps = {
 
 export function CustomTemplate({ curPage }: CustomTemplateProps) {
   const retroContext = useContext(RetrospectCreateContext);
-  return <ProgressBar curPage={curPage} lastPage={retroContext.totalStepsCnt}></ProgressBar>;
+  return (
+    <DefaultLayout LeftComp={<Icon icon={"ic_arrow_back"} onClick={retroContext.goPrev} />}>
+      <ProgressBar curPage={curPage} lastPage={retroContext.totalStepsCnt} />
+    </DefaultLayout>
+  );
 }
--- src/component/retrospectCreate/steps/DueDate.tsx
+++ src/component/retrospectCreate/steps/DueDate.tsx
@@ -1,7 +1,9 @@
 import { useContext } from "react";
 
 import { RetrospectCreateContext } from "@/app/retrospectCreate/RetrospectCreate";
+import { Icon } from "@/component/common/Icon";
 import { ProgressBar } from "@/component/common/ProgressBar";
+import { DefaultLayout } from "@/layout/DefaultLayout";
 
 type DueDateProps = {
   curPage: number;
@@ -9,5 +11,9 @@ type DueDateProps = {
 
 export function DueDate({ curPage }: DueDateProps) {
   const retroContext = useContext(RetrospectCreateContext);
-  return <ProgressBar curPage={curPage} lastPage={retroContext.totalStepsCnt}></ProgressBar>;
+  return (
+    <DefaultLayout LeftComp={<Icon icon={"ic_arrow_back"} onClick={retroContext.goPrev} />}>
+      <ProgressBar curPage={curPage} lastPage={retroContext.totalStepsCnt} />
+    </DefaultLayout>
+  );
 }
