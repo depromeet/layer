0e09731682db89ce3a2e4f2bb80973964312e320
이민희 <mhl98@dgu.ac.kr>
2024-07-18 15:04:27 +0900

    feat: #35 회고명, 한줄설명 입력 페이지

--- src/component/retrospectCreate/steps/MainInfo.tsx
+++ src/component/retrospectCreate/steps/MainInfo.tsx
@@ -1,18 +1,49 @@
+import { css } from "@emotion/react";
 import { useContext } from "react";
 
 import { RetrospectCreateContext } from "@/app/retrospectCreate/RetrospectCreate";
+import { ButtonProvider } from "@/component/common/button";
+import { Header } from "@/component/common/header";
+import { Input, InputLabelContainer, Label, TextArea } from "@/component/common/input";
 import { ProgressBar } from "@/component/common/ProgressBar";
+import { useInput } from "@/hooks/useInput";
 
 type MainInfoProps = {
   curPage: number;
 };
 
 export function MainInfo({ curPage }: MainInfoProps) {
-  const retroContext = useContext(RetrospectCreateContext);
+  const { goNext, totalStepsCnt } = useContext(RetrospectCreateContext);
+  const { value: retroName, handleInputChange: handleNameChange } = useInput();
+  const { value: description, handleInputChange: handleDescriptionChange } = useInput();
   return (
     <>
-      <ProgressBar curPage={curPage} lastPage={retroContext.totalStepsCnt} />
-      <div>main</div>
+      <ProgressBar curPage={curPage} lastPage={totalStepsCnt} />
+      <Header title={"회고의\n이름은 무엇인가요?"} />
+      <div
+        css={css`
+          display: flex;
+          flex-direction: column;
+          gap: 4rem;
+          margin-top: 7.5rem;
+        `}
+      >
+        <InputLabelContainer id="name">
+          <Label>회고 명</Label>
+          <Input value={retroName} onChange={handleNameChange} maxLength={10} count />
+        </InputLabelContainer>
+
+        <InputLabelContainer id="name">
+          <Label>한 줄 설명</Label>
+          <TextArea value={description} onChange={handleDescriptionChange} maxLength={20} count />
+        </InputLabelContainer>
+      </div>
+
+      <ButtonProvider>
+        <ButtonProvider.Primary onClick={goNext} disabled={!(retroName && description)}>
+          시작하기
+        </ButtonProvider.Primary>
+      </ButtonProvider>
     </>
   );
 }
