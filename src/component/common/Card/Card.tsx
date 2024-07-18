1228cb5daf5d9eef0228f7a2c52e045f22a4dddf
이민희 <mhl98@dgu.ac.kr>
2024-07-19 02:29:54 +0900

    fix: #11 카드 height 제거

--- src/component/common/Card/Card.tsx
+++ src/component/common/Card/Card.tsx
@@ -2,7 +2,6 @@ import { css, Interpolation, Theme } from "@emotion/react";
 
 type CardProps = {
   width?: string;
-  height?: string;
   rounded?: keyof typeof BORDER_RADIUS;
   shadow?: boolean;
   padding?: string;
@@ -15,13 +14,12 @@ const BORDER_RADIUS = {
   md: "1.2rem",
 } as const;
 
-export function Card({ width = "100%", height = "34rem", rounded = "sm", shadow = true, padding = "2rem", styles, children }: CardProps) {
+export function Card({ width = "100%", rounded = "sm", shadow = true, padding = "2rem", styles, children }: CardProps) {
   return (
     <div
       css={[
         css`
           width: ${width};
-          height: ${height};
           border-radius: ${BORDER_RADIUS[rounded]};
           background-color: #fff; // FIXME - design token
           box-shadow: ${shadow ? "0 .4rem 1.2rem rgba(0 0 0 / 4%)" : "none"};
