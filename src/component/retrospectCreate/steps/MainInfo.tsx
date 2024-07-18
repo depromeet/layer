3a8d7c10c012e36b03a57de67f36a592f0792602
이민희 <mhl98@dgu.ac.kr>
2024-07-19 01:20:28 +0900

    feat: #35 팁 추가

--- src/component/retrospectCreate/steps/MainInfo.tsx
+++ src/component/retrospectCreate/steps/MainInfo.tsx
@@ -5,6 +5,7 @@ import { RetrospectCreateContext } from "@/app/retrospectCreate/RetrospectCreate
 import { ButtonProvider } from "@/component/common/button";
 import { Header } from "@/component/common/header";
 import { Input, InputLabelContainer, Label, TextArea } from "@/component/common/input";
+import { TipCard } from "@/component/common/tip/TipCard";
 import { useInput } from "@/hooks/useInput";
 
 export function MainInfo() {
@@ -27,10 +28,18 @@ export function MainInfo() {
           <Input value={retroName} onChange={handleNameChange} maxLength={10} count />
         </InputLabelContainer>
 
-        <InputLabelContainer id="name">
-          <Label>한 줄 설명</Label>
-          <TextArea value={description} onChange={handleDescriptionChange} maxLength={20} count />
-        </InputLabelContainer>
+        <div>
+          <InputLabelContainer id="name">
+            <Label>한 줄 설명</Label>
+            <TextArea value={description} onChange={handleDescriptionChange} maxLength={20} count />
+          </InputLabelContainer>
+          <TipCard
+            message="회고 설명 또는 진행 목표에 대해 적어도 좋아요 :)"
+            styles={css`
+              margin-top: 0.8rem;
+            `}
+          />
+        </div>
       </div>
 
       <ButtonProvider>
