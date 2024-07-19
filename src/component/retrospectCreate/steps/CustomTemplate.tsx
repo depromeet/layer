6653e64c69b82ce7a5e01f449b7b3ec55ac44de1
이민희 <mhl98@dgu.ac.kr>
2024-07-19 16:56:38 +0900

    feat: #35 질문 리스트 페이지 생성

--- src/component/retrospectCreate/steps/CustomTemplate.tsx
+++ src/component/retrospectCreate/steps/CustomTemplate.tsx
@@ -1,6 +1,7 @@
 import { css } from "@emotion/react";
 import { useAtom } from "jotai";
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 import { RetrospectCreateContext } from "@/app/retrospectCreate/RetrospectCreate";
 import { ButtonProvider } from "@/component/common/button";
@@ -11,6 +12,7 @@ import { Spacing } from "@/component/common/Spacing";
 import { Tag } from "@/component/common/tag";
 import { Typography } from "@/component/common/typography";
 import { OrderLabel } from "@/component/retrospectCreate/OrderLabel";
+import { PATHS } from "@/config/paths";
 import { questionsAtom } from "@/store/retrospect/retrospectCreate";
 
 //FIXME - 질문 더미 데이터
@@ -19,6 +21,7 @@ import { questionsAtom } from "@/store/retrospect/retrospectCreate";
 export function CustomTemplate() {
   const retroContext = useContext(RetrospectCreateContext);
   const [questions, _] = useAtom(questionsAtom);
+  const navigate = useNavigate();
   return (
     <>
       <Header title={"해당 템플릿으로\n수정 없이 회고를 진행할까요?"} contents={"질문을 추가하거나 뺄 수 있어요!"} />
@@ -46,7 +49,7 @@ export function CustomTemplate() {
         <Spacing size={3} />
       </Card>
       <ButtonProvider sort={"horizontal"}>
-        <ButtonProvider.Gray>질문 수정</ButtonProvider.Gray>
+        <ButtonProvider.Gray onClick={() => navigate(PATHS.questionsEdit())}>질문 수정</ButtonProvider.Gray>
         <ButtonProvider.Primary onClick={retroContext.goNext}>이대로 작성</ButtonProvider.Primary>
       </ButtonProvider>
     </>
