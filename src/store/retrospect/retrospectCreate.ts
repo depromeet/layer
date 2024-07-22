1fe17f63ccf50a92cb19c1985872e533b64c3684
이민희 <mhl98@dgu.ac.kr>
2024-07-22 23:55:43 +0900

    feat: #35 회고명, 한줄소개 상태 저장

--- src/store/retrospect/retrospectCreate.ts
+++ src/store/retrospect/retrospectCreate.ts
@@ -1,7 +1,14 @@
 import { atom } from "jotai";
 
-export const questionsAtom = atom([
+import { MainInfo, Questions } from "@/types/retrospectCreate";
+
+export const questionsAtom = atom<Questions>([
   "계속 유지하고 싶은 것은 무엇인가요?",
   "어려움을 느꼈던 부분은 무엇인가요?",
   "새롭게 시도해볼 내용은 무엇인가요?",
 ]);
+
+export const mainInfoAtom = atom<MainInfo>({
+  title: "",
+  introduction: "",
+});
