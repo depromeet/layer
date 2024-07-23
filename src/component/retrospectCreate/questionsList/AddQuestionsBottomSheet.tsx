72a4009c5db65a39ce5c1bf3b8d070441c785b36
이민희 <mhl98@dgu.ac.kr>
2024-07-23 20:10:07 +0900

    fix: #35 바텀시트 직접 작성한 질문 상태 반영

--- src/component/retrospectCreate/questionsList/AddQuestionsBottomSheet.tsx
+++ src/component/retrospectCreate/questionsList/AddQuestionsBottomSheet.tsx
@@ -1,35 +1,39 @@
 import { css } from "@emotion/react";
 import { useAtom } from "jotai";
-import { useState } from "react";
 
 import { SelectRecommended } from "./SelectRecommended";
 import { Tab } from "./Tab";
 
 import { ButtonProvider } from "@/component/common/button";
-import { TextArea } from "@/component/common/input";
+import { Input } from "@/component/common/input";
 import { useCheckBox } from "@/hooks/useCheckBox";
+import { useInput } from "@/hooks/useInput";
 import { useTabs } from "@/hooks/useTabs";
 import { questionsAtom } from "@/store/retrospect/retrospectCreate";
 
-export function AddQuestionsBottomSheet() {
+type AddQuestionsBottomSheetProps = {
+  onClose: () => void;
+};
+
+export function AddQuestionsBottomSheet({ onClose }: AddQuestionsBottomSheetProps) {
   const { tabs, curTab, selectTab } = useTabs(["직접 작성", "추천 질문"] as const);
   const [_, setQuestions] = useAtom(questionsAtom);
-  const [createdQuestion, setCreatedQuestion] = useState("");
+  const { value: customQuestion, handleInputChange: handleCustomChange, resetInput } = useInput();
   const { isChecked, toggle, selectedValues } = useCheckBox();
 
-  const handleCreateQuestion = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setCreatedQuestion(e.target.value);
-  };
-
   const handleCustomSave = () => {
-    setQuestions((prev) => [...prev, createdQuestion]);
+    setQuestions((prev) => [...prev, customQuestion]);
+    resetInput();
+    onClose();
   };
 
   const handleRecommendedSave = () => {
     setQuestions((prev) => [...prev, ...selectedValues]);
+    onClose();
   };
+
   return (
-    <>
+    <div>
       <Tab tabs={tabs} curTab={curTab} selectTab={selectTab} />
       {curTab === "직접 작성" && (
         <div
@@ -37,7 +41,7 @@ export function AddQuestionsBottomSheet() {
             margin-top: 2.3rem;
           `}
         >
-          <TextArea placeholder="질문을 작성해주세요." value={createdQuestion} onChange={handleCreateQuestion} maxLength={10} count />
+          <Input placeholder="질문을 작성해주세요." value={customQuestion} onChange={handleCustomChange} maxLength={10} count />
           <ButtonProvider>
             <ButtonProvider.Primary onClick={handleCustomSave}>추가하기</ButtonProvider.Primary>
           </ButtonProvider>
@@ -47,10 +51,10 @@ export function AddQuestionsBottomSheet() {
         <div>
           <SelectRecommended checkBoxHandlers={{ isChecked, toggle }} />
           <ButtonProvider>
-            <ButtonProvider.Primary onClick={handleRecommendedSave}>추가하기</ButtonProvider.Primary>
+            <ButtonProvider.Primary onClick={handleRecommendedSave}>{`추가하기`}</ButtonProvider.Primary>
           </ButtonProvider>
         </div>
       )}
-    </>
+    </div>
   );
 }
