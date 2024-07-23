8015058739973bb3dbfd6fb885a3d88f1ff0919e
이민희 <mhl98@dgu.ac.kr>
2024-07-23 22:22:20 +0900

    fix: #35 추천 질문 데이터 작성, 체크박스 변경

--- src/hooks/useTabs.ts
+++ src/hooks/useTabs.ts
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export const useTabs = <T extends string>(tabs: T[]) => {
+export const useTabs = <T extends string>(tabs: readonly T[]) => {
   const [curTab, setCurTab] = useState(tabs[0]);
 
   const selectTab = (tab: T) => {
