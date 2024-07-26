507d59e671e203581c093bac808c33704467c6d8
이민희 <mhl98@dgu.ac.kr>
2024-07-26 15:46:17 +0900

    fix: #35 아이콘 부모 높이 제거

--- src/assets/svgs/common/index.ts
+++ src/assets/svgs/common/index.ts
@@ -8,7 +8,8 @@ export { default as ic_chart } from "./ic_chart.svg?react";
 
 export { default as basicProfile } from "./basicProfile.svg?react";
 export { default as ic_arrow_back } from "./ic_arrow_back.svg?react";
-export { default as ic_delete_pink } from "./ic_delete_pink.svg?react";
+export { default as ic_delete } from "./ic_delete.svg?react";
 export { default as ic_handle } from "./ic_handle.svg?react";
 export { default as ic_pencil } from "./ic_pencil.svg?react";
 export { default as ic_chevron_down } from "./ic_chevron_down.svg?react";
+export { default as ic_plus_thin } from "./ic_plus_thin.svg?react";
