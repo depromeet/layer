86f596f6b9ca3d06abe667502412ab3018c2ac25
donghunee <ehdgns1766@naver.com>
2024-07-29 20:06:19 +0900

    feat: #35 space 버튼 컴포넌트 생성

---
+++ src/component/space/TypeButton.tsx
@@ -0,0 +1,49 @@
+import { css } from "@emotion/react";
+
+import { Icon } from "@/component/common/Icon";
+import { Spacing } from "@/component/common/Spacing";
+
+type TypeButtonProps = {
+  isClicked?: boolean;
+  label: string;
+  onClick: () => void;
+};
+
+export function TypeButton({ isClicked = true, label, onClick }: TypeButtonProps) {
+  return (
+    <div
+      onClick={onClick}
+      css={css`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: ${isClicked ? "#6C9CFA" : "#f6f8fa"};
+        border-radius: 1.2rem;
+        cursor: pointer;
+        flex: 1;
+        padding: 4rem;
+        transition: 0.2s all;
+      `}
+    >
+      <div
+        css={css`
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          color: ${isClicked ? "#f6f8fa" : "#495057"};
+        `}
+      >
+        <Icon
+          icon="ic_human"
+          size={4}
+          color={!isClicked ? "#6C9CFA" : "#f6f8fa"}
+          css={css`
+            cursor: pointer;
+          `}
+        />
+        <Spacing size={2} />
+        <div>{label}</div>
+      </div>
+    </div>
+  );
+}
