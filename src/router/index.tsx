13dd45978f7aca1a80c163847495828412a82b70
sean <sean2337@naver.com>
2024-08-01 01:07:15 +0900

    feat: #35 라우팅 로그인 적용

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -46,12 +46,12 @@ const routerChildren: RouteChildren[] = [
   {
     path: "/space/create",
     element: <CreateSpacePage />,
-    auth: false,
+    auth: true,
   },
   {
     path: "/space/create/done",
     element: <CreateDonePage />,
-    auth: false,
+    auth: true,
   },
   {
     path: "/space/:spaceId",
