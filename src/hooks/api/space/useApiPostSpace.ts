1c74fd6fcba70c7b27f1c845081b893b5b24302e
donghunee <ehdgns1766@naver.com>
2024-07-31 14:28:32 +0900

    refactor: #35 onSuccess 로직 변경

--- src/hooks/api/space/useApiPostSpace.ts
+++ src/hooks/api/space/useApiPostSpace.ts
@@ -1,9 +1,15 @@
 import { useMutation } from "@tanstack/react-query";
+import { useResetAtom } from "jotai/utils";
+import { useNavigate } from "react-router-dom";
 
 import { api } from "@/api";
+import { spaceState } from "@/store/space/spaceAtom";
 import { SpaceValue } from "@/types/space";
 
 export const useApiPostSpace = () => {
+  const resetSpaceValue = useResetAtom(spaceState);
+  const navigate = useNavigate();
+
   const postSpace = async (formData: SpaceValue) => {
     const { imgUrl, category, field, name, introduction } = formData;
     const res = await api.post(`/api/space`, {
@@ -18,6 +24,10 @@ export const useApiPostSpace = () => {
 
   return useMutation({
     mutationFn: (formData: SpaceValue) => postSpace(formData),
+    onSuccess: () => {
+      navigate("/space/create/done");
+      resetSpaceValue();
+    },
     onError: (error) => {
       console.log(error);
     },
