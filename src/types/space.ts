d75a05fd4386fa7ed90946e06f92e43a282d37fa
donghunee <ehdgns1766@naver.com>
2024-07-31 15:01:02 +0900

    refactor: #35 field 상수 제거

--- src/types/space.ts
+++ src/types/space.ts
@@ -1,19 +1,6 @@
-export const fieldArr = ["PLANNER", "EDUCATION", "DEVELOPMENT", "DESIGN", "MANAGEMENT", "DATA_ANALYSIS", "MARKETING", "RESEARCH", "ETC"] as const;
-export type FieldType = (typeof fieldArr)[number];
+import { fieldArr } from "@/component/space/field.const";
 
-export const fieldMap: {
-  [key in FieldType]: string;
-} = {
-  PLANNER: "기획",
-  EDUCATION: "교육",
-  DEVELOPMENT: "개발",
-  DESIGN: "디자인",
-  MANAGEMENT: "운영 및 관리",
-  DATA_ANALYSIS: "데이터 분석",
-  MARKETING: "마케팅",
-  RESEARCH: "연구",
-  ETC: "기타",
-};
+export type FieldType = (typeof fieldArr)[number];
 
 export type SpaceValue = {
   category: ProjectType;
