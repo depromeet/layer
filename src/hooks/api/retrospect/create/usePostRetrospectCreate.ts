f426a2c51c4b712147d585f8f3bd12473c13f36e
이민희 <mhl98@dgu.ac.kr>
2024-08-02 17:02:42 +0900

    refactor: #35 response 타입 정리

--- src/hooks/api/retrospect/create/usePostRetrospectCreate.ts
+++ src/hooks/api/retrospect/create/usePostRetrospectCreate.ts
@@ -1,6 +1,4 @@
 import { useMutation } from "@tanstack/react-query";
-import { AxiosResponse } from "axios";
-import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
 
 import { api } from "@/api";
@@ -8,21 +6,19 @@ import { RetrospectCreateReq } from "@/types/retrospectCreate";
 
 type PostRetrospect = { spaceId: number; body: RetrospectCreateReq };
 
+type RetrospectCreateRes = { retrospectId: number };
+
 export const usePostRetrospectCreate = (spaceId: number) => {
   const navigate = useNavigate();
 
-  const postRetrospect = async ({ spaceId, body }: PostRetrospect): Promise<AxiosResponse<{ retrospectId: number }>> => {
-    const data = await api.post(`/space/${spaceId}/retrospect`, body, {
-      headers: {
-        Authorization: Cookies.get("accessToken"),
-      },
-    });
-    return data;
+  const postRetrospect = async ({ spaceId, body }: PostRetrospect): Promise<RetrospectCreateRes> => {
+    const res = await api.post(`/space/${spaceId}/retrospect`, body);
+    return res.data as RetrospectCreateRes;
   };
 
   return useMutation({
     mutationFn: postRetrospect,
-    onSuccess: ({ data: { retrospectId } }) => {
+    onSuccess: ({ retrospectId }) => {
       navigate(`/write`, {
         state: { retrospectId, spaceId },
       });
