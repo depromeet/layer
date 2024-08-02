0722232ab221ca561faac49ce1d6a5b81db78b91
sean <sean2337@naver.com>
2024-08-02 22:16:30 +0900

    feat:#35: MidModal 컴포넌트 생성

--- src/app/space/SpaceViewPage.tsx
+++ src/app/space/SpaceViewPage.tsx
@@ -3,14 +3,18 @@ import { Fragment, useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
 import { spaceRestrospectFetch, spaceInfoFetch } from "@/api/Retrospect";
+import { retrospectDelete } from "@/api/space";
 import { BottomSheet } from "@/component/BottomSheet";
 import { Icon } from "@/component/common/Icon";
+import { MidModal } from "@/component/common/Modal/MidModal";
 import { Spacing } from "@/component/common/Spacing";
+import { Toast } from "@/component/common/Toast";
 import { Typography } from "@/component/common/typography";
 import { SpaceCountView, RetrospectBox, TeamGoalView, CreateRetrospectiveSheet } from "@/component/space";
 import { EmptyRetrospect } from "@/component/space/view/EmptyRetrospect";
 import { SpaceAppBarRightComp } from "@/component/space/view/SpaceAppBarRightComp";
 import { useBottomSheet } from "@/hooks/useBottomSheet";
+import { useToast } from "@/hooks/useToast";
 import { DefaultLayout } from "@/layout/DefaultLayout";
 import { DESIGN_SYSTEM_COLOR } from "@/style/variable";
 import { Space } from "@/types/spaceType";
@@ -28,11 +32,12 @@ type RestrospectType = {
 export function SpaceViewPage() {
   const { spaceId } = useParams<{ spaceId: string }>();
   const { openBottomSheet } = useBottomSheet();
-  const naviagte = useNavigate();
+  const navigate = useNavigate();
   const [layerCount, setLayerCount] = useState<number>(0);
   const [spaceInfo, setSpaceInfo] = useState<Space>();
   const [restrospectArr, setRestrospectArr] = useState<RestrospectType[]>([]);
-
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const { toast } = useToast();
   useEffect(() => {
     if (spaceId) {
       spaceInfoFetch(Number(spaceId))
@@ -41,7 +46,7 @@ export function SpaceViewPage() {
         })
         .catch((error) => {
           console.error(error);
-          naviagte(-1);
+          navigate(-1);
         });
 
       spaceRestrospectFetch(Number(spaceId))
@@ -51,7 +56,7 @@ export function SpaceViewPage() {
         })
         .catch((error) => {
           console.error(error);
-          naviagte(-1);
+          navigate(-1);
         });
     }
   }, [spaceId]);
@@ -59,8 +64,41 @@ export function SpaceViewPage() {
   const proceedingRetrospects = restrospectArr.filter((retrospect) => retrospect.retrospectStatus === "PROCEEDING");
   const doneRetrospects = restrospectArr.filter((retrospect) => retrospect.retrospectStatus === "DONE");
 
+  // 스페이스 삭제 버튼 함수
+  const handleDeleteFun = async () => {
+    await retrospectDelete(spaceId)
+      .then(() => {
+        navigate("/home/retrospect");
+      })
+      .catch((err) => {
+        console.error("삭제 실패:", err);
+        //FIXME: 팀장은 삭제하는가? 정책 논의 필요
+        toast.error("스페이스 삭제에 실패하였습니다");
+      })
+      .finally(() => {
+        setIsModalVisible(false);
+      });
+  };
+
+  //모달 오픈 함수
+  const handleOpenModal = () => {
+    setIsModalVisible(true);
+  };
+
+  //모든 클로스 함수
+  const handleCloseModal = () => {
+    setIsModalVisible(false);
+  };
+
   return (
-    <DefaultLayout theme="dark" height="6.4rem" title={spaceInfo?.name} RightComp={<SpaceAppBarRightComp spaceId={spaceId} />}>
+    <DefaultLayout
+      theme="dark"
+      height="6.4rem"
+      title={spaceInfo?.name}
+      RightComp={<SpaceAppBarRightComp spaceId={spaceId} onDeleteClick={handleOpenModal} />}
+    >
+      <Toast />
+
       <TeamGoalView />
       <Spacing size={1.1} />
       <SpaceCountView memberCount={spaceInfo?.memberCount} layerCount={layerCount} />
@@ -158,6 +196,17 @@ export function SpaceViewPage() {
         }
         handler={true}
       />
+      {isModalVisible && (
+        <MidModal
+          title="스페이스를 삭제하시겠어요?"
+          content="스페이스를 다시 되돌릴 수 없어요"
+          leftFun={handleCloseModal}
+          rightFun={handleDeleteFun}
+          onClose={() => {
+            setIsModalVisible(false);
+          }}
+        />
+      )}
     </DefaultLayout>
   );
 }
---
+++ src/component/common/Modal/MidModal.tsx
@@ -0,0 +1,119 @@
+import { css } from "@emotion/react";
+import { useRef } from "react";
+
+import { Button, ButtonProvider } from "@/component/common/button";
+import { HeaderProvider } from "@/component/common/header";
+import { Spacing } from "@/component/common/Spacing";
+import { ANIMATION } from "@/style/common/animation.ts";
+
+type MidModalProps = {
+  title: string;
+  content: string;
+  leftText?: string;
+  rightText?: string;
+  leftFun?: () => void;
+  rightFun?: () => void;
+  onClose?: () => void;
+};
+
+export function MidModal({ title, content, leftText = "취소", rightText = "삭제", leftFun, rightFun, onClose }: MidModalProps) {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  const handleBackgroundClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.target === containerRef.current) {
+      onClose?.();
+    }
+  };
+
+  return (
+    <div
+      css={css`
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(6, 8, 12, 0.72);
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        box-sizing: border-box;
+        z-index: 99999;
+      `}
+      ref={containerRef}
+      onClick={handleBackgroundClick}
+    >
+      <div
+        css={css`
+          width: 100%;
+          height: fit-content;
+          max-width: 46rem;
+          max-height: 46rem;
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          transition: 0.4s all;
+          animation: ${ANIMATION.FADE_IN} 0.4s ease-in-out;
+          padding: 2rem;
+        `}
+      >
+        <div
+          css={css`
+            width: 100%;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            background-color: white;
+            border-radius: 1.2rem;
+            padding: 3rem 2rem;
+            row-gap: 2.8rem;
+            box-sizing: border-box;
+          `}
+        >
+          <HeaderProvider>
+            <HeaderProvider.Subject
+              contents={title}
+              css={css`
+                font-size: 1.8rem;
+              `}
+            />
+            <Spacing size={0.1} />
+            <HeaderProvider.Description
+              contents={content}
+              css={css`
+                font-size: 1.6rem;
+              `}
+            />
+          </HeaderProvider>
+          <div
+            css={css`
+              width: 100%;
+            `}
+          >
+            <ButtonProvider
+              sort={"horizontal"}
+              onlyContainerStyle={css`
+                padding: 0;
+                div:nth-of-type(1) {
+                  display: none;
+                }
+              `}
+            >
+              <Button colorSchema={"gray"} onClick={leftFun}>
+                {leftText}
+              </Button>
+              <Button colorSchema={"primary"} onClick={rightFun}>
+                {rightText}
+              </Button>
+            </ButtonProvider>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
