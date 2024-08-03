4455cbb0cac4ab8bf325f94eb20849841904fd6e
이민희 <mhl98@dgu.ac.kr>
2024-08-04 03:53:15 +0900

    fix: #35 history state 형태 수정

--- src/app/space/CreateNextPage.tsx
+++ src/app/space/CreateNextPage.tsx
@@ -49,7 +49,9 @@ export function CreateNextPage() {
         />
       </div>
       <ButtonProvider>
-        <ButtonProvider.Primary onClick={() => navigate(PATHS.retrospectCreate(), { state: spaceId })}>회고 템플릿 추천 받기</ButtonProvider.Primary>
+        <ButtonProvider.Primary onClick={() => navigate(PATHS.retrospectCreate(), { state: { spaceId } })}>
+          회고 템플릿 추천 받기
+        </ButtonProvider.Primary>
         <div css={laterTextStyles}>다음에 하기</div>
       </ButtonProvider>
     </DefaultLayout>
