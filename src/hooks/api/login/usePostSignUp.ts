f2d789b6fb20bf9a7f63be461ba0129f7fd7a602
sean <sean2337@naver.com>
2024-08-07 23:17:54 +0900

    feat#35 SignUp complete

---
+++ src/hooks/api/login/usePostSignUp.ts
@@ -0,0 +1,44 @@
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+
+import { api } from "@/api";
+import { PATHS } from "@/config/paths";
+import { useToast } from "@/hooks/useToast";
+import { AuthResponse } from "@/types/loginType";
+
+type PostSignUp = {
+  accessToken: string;
+  name: string;
+};
+
+export const usePostSignUp = () => {
+  const navigate = useNavigate();
+  const { toast } = useToast();
+  const signUpWithToken = async ({ accessToken, name }: PostSignUp): Promise<AuthResponse> => {
+    const response = await api.post<AuthResponse>(
+      "/api/auth/sign-up",
+      {
+        socialType: "KAKAO",
+        name: name,
+      },
+      {
+        headers: {
+          "X-Auth-Token": accessToken,
+        },
+      },
+    );
+    return response.data;
+  };
+
+  return useMutation<AuthResponse, unknown, PostSignUp>({
+    mutationFn: signUpWithToken,
+    onSuccess: () => {
+      toast.success("Layer에 온걸 환영해요!");
+      navigate(PATHS.home());
+    },
+    onError: (error) => {
+      console.error("Sign up failed:", error);
+      toast.error("회원가입이 실패했습니다.");
+    },
+  });
+};
