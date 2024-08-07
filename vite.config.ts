360177eaa043037bfec40787eea7183d25a6a382
klmhyeonwoo <gentlemonster77@likelion.org>
2024-08-08 00:51:45 +0900

    chore: #83 Add SEO essential elements

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import Sitemap from "vite-plugin-sitemap";
 import svgr from "vite-plugin-svgr";
 import path from "path";
 
@@ -13,6 +14,7 @@ export default defineConfig({
       },
     }),
     svgr(),
+    Sitemap(),
   ],
   resolve: {
     alias: {
