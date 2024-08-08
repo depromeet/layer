d6228c91401527533e713786add89a52133879e6
이민희 <mhl98@dgu.ac.kr>
2024-08-08 21:36:59 +0900

    feat: #88 textarea에도 동일한 validation 기능 추가

--- src/component/common/input/TextArea.tsx
+++ src/component/common/input/TextArea.tsx
@@ -2,60 +2,102 @@ import { css } from "@emotion/react";
 import { forwardRef, useContext, useState } from "react";
 
 import { InputContext } from "./InputLabelContainer";
+import { patterns } from "./patterns.const";
 
 import { Typography } from "@/component/common/typography";
 import { DESIGN_SYSTEM_COLOR } from "@/style/variable";
 
 type TextAreaProps = {
   value: string;
-  onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
   width?: string;
   height?: string;
   count?: boolean;
+  onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  validations?: (keyof typeof patterns)[];
 } & React.TextareaHTMLAttributes<HTMLTextAreaElement>;
 
-export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(function ({ id, width = "100%", height = "8.4rem", count, ...props }, ref) {
+export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(function (
+  { id, width = "100%", height = "8.4rem", count, validations, onChange, ...props },
+  ref,
+) {
   const { maxLength, value } = props;
   const [isFocused, setIsFocused] = useState(false);
   const textareaContext = useContext(InputContext);
+  const [errorMsg, setErrorMsg] = useState<string>();
+
+  const onInputValidate = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    if (!validations) return;
+    for (let i = 0; i < validations.length; i++) {
+      const validation = validations[i];
+      const pattern = new RegExp(patterns[validation]["pattern"], "g");
+      const isValid = pattern.test(e.target.value);
+      if (isValid) {
+        setErrorMsg(undefined);
+        return;
+      } else {
+        setErrorMsg(patterns[validation]["errorMsg"]);
+        return;
+      }
+    }
+  };
+
   return (
-    <div
-      css={css`
-        width: ${width};
-        border: 1px solid;
-        border-color: ${isFocused ? DESIGN_SYSTEM_COLOR.theme3 : "#e3e6ea"}; // FIXME: 디자인 토큰 적용하기
-        border-radius: 0.8rem;
-        padding: 1.6rem;
-        display: flex;
-        flex-direction: column;
-        height: ${height};
-        transition: 0.2s all;
-      `}
-    >
-      <textarea
-        ref={ref}
-        id={id || textareaContext?.id}
+    <div>
+      <div
         css={css`
-          ::placeholder {
-            color: ${DESIGN_SYSTEM_COLOR.lightGrey5};
-          }
-          flex-shrink: 0;
-          flex-grow: 1;
+          width: ${width};
+          border: 1px solid;
+          border-color: ${isFocused ? DESIGN_SYSTEM_COLOR.theme3 : "#e3e6ea"}; // FIXME: 디자인 토큰 적용하기
+          border-radius: 0.8rem;
+          padding: 1.6rem;
+          display: flex;
+          flex-direction: column;
+          height: ${height};
+          transition: 0.2s all;
         `}
-        onFocus={() => setIsFocused(true)}
-        onBlur={() => setIsFocused(false)}
-        {...props}
-      />
-      {count && maxLength && (
+      >
+        <textarea
+          ref={ref}
+          id={id || textareaContext?.id}
+          css={css`
+            ::placeholder {
+              color: ${DESIGN_SYSTEM_COLOR.lightGrey5};
+            }
+            flex-shrink: 0;
+            flex-grow: 1;
+          `}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          onChange={(e) => {
+            onChange(e);
+            if (validations && validations.length > 0) {
+              onInputValidate(e);
+            }
+          }}
+          {...props}
+        />
+        {count && maxLength && (
+          <div
+            css={css`
+              align-self: flex-end;
+            `}
+          >
+            <Typography variant="CAPTION" color={value.length ? "theme3" : "lightGrey"}>
+              {value.length}
+            </Typography>
+            <Typography variant="CAPTION" color={"lightGrey"}>{`/${maxLength}`}</Typography>
+          </div>
+        )}
+      </div>
+      {errorMsg && (
         <div
           css={css`
-            align-self: flex-end;
+            margin-top: 0.8rem;
           `}
         >
-          <Typography variant="CAPTION" color={value.length ? "theme3" : "lightGrey"}>
-            {value.length}
+          <Typography color="red400" variant="B2">
+            {`*${errorMsg}`}
           </Typography>
-          <Typography variant="CAPTION" color={"lightGrey"}>{`/${maxLength}`}</Typography>
         </div>
       )}
     </div>
