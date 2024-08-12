13506ab82a90e151aa631d37720a848c4af1948c
sean <sean2337@naver.com>
2024-08-12 14:17:46 +0900

    design:#88 Add Text DesignToken

--- src/style/designTokens.ts
+++ src/style/designTokens.ts
@@ -4,6 +4,11 @@ export const DESIGN_TOKEN_TEXT = {
     fontWeight: "700",
     lineHeight: "150%",
   },
+  title20Bold: {
+    fontSize: "2.0rem",
+    fontWeight: "700",
+    lineHeight: "140%",
+  },
   title18Bold: {
     fontSize: "1.8rem",
     fontWeight: "700",
