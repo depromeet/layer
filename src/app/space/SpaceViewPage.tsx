20278b1f8bac1b284eda0f931fc85182e474810d
sean <sean2337@naver.com>
2024-08-16 00:32:57 +0900

    fix#118 회고 박스 상태 버그 수정

--- src/app/space/SpaceViewPage.tsx
+++ src/app/space/SpaceViewPage.tsx
@@ -120,6 +120,7 @@ export function SpaceViewPage() {
             </div>
             <div
               css={css`
+                margin-top: 1.6rem;
                 display: flex;
                 flex-direction: column;
                 gap: 1rem;
