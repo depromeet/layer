af29b25d04d9eda5d69c95409e06fbb1e16a9d27
donghunee <ehdgns1766@naver.com>
2024-08-15 23:18:18 +0900

    feat: #35 invite toast message 추가

--- src/hooks/api/space/useApiJoinSpace.ts
+++ src/hooks/api/space/useApiJoinSpace.ts
@@ -2,10 +2,11 @@ import { useMutation } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 
 import { api } from "@/api";
+import { useToast } from "@/hooks/useToast";
 
 export const useApiJoinSpace = () => {
   const navigate = useNavigate();
-
+  const { toast } = useToast();
   const joinSpace = async (spaceId: number) => {
     const res = await api.post(`/api/space/join`, null, { params: { spaceId } });
     return res;
@@ -14,6 +15,7 @@ export const useApiJoinSpace = () => {
   return useMutation({
     mutationFn: (spaceId: number) => joinSpace(spaceId),
     onSuccess: () => {
+      toast.success("팀에 초대 되었습니다.");
       navigate("/space/create/done");
     },
     onError: (error: { status: number }) => {
