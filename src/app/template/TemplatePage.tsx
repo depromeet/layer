34978149c085dc94c3a706e80855ba55f9c5c1ab
klmhyeonwoo <gentlemonster77@likelion.org>
2024-08-17 12:30:35 +0900

    feat: #35 Add Template Page Data Fetching

--- src/app/template/TemplatePage.tsx
+++ src/app/template/TemplatePage.tsx
@@ -1,5 +1,6 @@
 import { css } from "@emotion/react";
-import { useEffect, useRef, useState } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 import { Button, ButtonProvider } from "@/component/common/button";
 import { Icon } from "@/component/common/Icon";
@@ -9,12 +10,26 @@ import { ExampleButton } from "@/component/template/ExampleButton.tsx";
 import { PurposeBox } from "@/component/template/PurposeBox.tsx";
 import { QuestionBox } from "@/component/template/QuestionBox.tsx";
 import { TipBox } from "@/component/template/TipBox.tsx";
+import { useGetTemplateInfo } from "@/hooks/api/template/useGetTemplateInfo.ts";
 import { TemplateLayout } from "@/layout/TemplateLayout.tsx";
 
 export function TemplatePage() {
   const appbarRef = useRef<HTMLDivElement>(null);
   const contentRef = useRef<HTMLDivElement>(null);
   const [isColliding, setIsColliding] = useState(false);
+  const location = useLocation();
+  const { templateId } = location?.state as { templateId: number };
+  /**
+   * template
+   * - 10000 : KPT
+   * - 10001 : 5F
+   * - 10002 : Mad Sad Glad
+   * - 10003 : SSC
+   * - 10004 : PMI
+   * - 10005 : 무제
+   * */
+  // const templateSet = [10000, 10001, 10002, 10003, 10004, 10005];
+  const { data } = useGetTemplateInfo({ templateId: templateId });
 
   const checkCollision = () => {
     if (appbarRef.current && contentRef.current) {
@@ -36,166 +51,156 @@ export function TemplatePage() {
   }, []);
 
   return (
-    <TemplateLayout theme={isColliding ? "default" : "transparent"}>
-      <TemplateLayout.Header
-        LeftComp={
-          <Icon
-            icon={"ic_arrow_back_white"}
-            css={css`
-              path {
-                fill: ${isColliding ? "#212329" : "fff"};
-                transition: 0.4s all;
-              }
-            `}
-          />
-        }
-        ref={appbarRef}
-      ></TemplateLayout.Header>
-      <TemplateLayout.Main>
-        <section
-          css={css`
-            padding-top: 2rem;
-            display: flex;
-            flex-direction: column;
-            row-gap: 6rem;
-          `}
-          ref={contentRef}
-        >
-          <article
-            css={css`
-              display: flex;
-              flex-direction: column;
-              row-gap: 1.2rem;
-            `}
-          >
-            <span
-              id="title"
+    <Fragment>
+      <TemplateLayout theme={isColliding ? "default" : "transparent"}>
+        <TemplateLayout.Header
+          LeftComp={
+            <Icon
+              icon={"ic_arrow_back_white"}
               css={css`
-                font-size: 2.4rem;
-                font-weight: 600;
-              `}
-            >
-              감정 통찰을 위한 회고
-            </span>
-            <div
-              css={css`
-                div {
-                  width: fit-content;
+                path {
+                  fill: ${isColliding ? "#212329" : "fff"};
+                  transition: 0.4s all;
                 }
               `}
-            >
-              <TagBox tagName={"테스트"} />
-            </div>
-            <div
-              id="description"
-              css={css`
-                color: #454952;
-                line-height: 1.5;
-              `}
-            >
-              MAD SAD GLAD 회고법은 구성원 스스로 어떤 감정을 가지고 있는지 고민할 수 있도록 돕고 서로 정서적인 경험을 공유하며 팀의 결속력을 높이는데
-              유용해요.
-            </div>
-          </article>
-          <article>
-            <span
-              css={css`
-                color: #212329;
-                font-weight: 600;
-                font-size: 1.8rem;
-              `}
-            >
-              이런 목적으로 사용하기 좋아요
-            </span>
-            <Spacing size={2} />
-            <div
-              css={css`
-                display: flex;
-                align-items: center;
-                column-gap: 0.8rem;
-                row-gap: 0.8rem;
-                flex-wrap: wrap;
-              `}
-            >
-              {/* FIXME: 데이터가 반복으로 들어가는 구간, 데이터 패칭 시 수정 */}
-              <PurposeBox purpose={"팀워크 강화"} />
-              <PurposeBox purpose={"팀워크 강화"} />
-              <PurposeBox purpose={"김현도리 김현도리 김현도리"} />
-              <PurposeBox purpose={"팀워크 강화"} />
-              <PurposeBox purpose={"김현도리 김현도리 김현도리"} />
-            </div>
-          </article>
-          <article>
-            <span
-              css={css`
-                color: #212329;
-                font-weight: 600;
-                font-size: 1.8rem;
-              `}
-            >
-              이런 목적으로 사용하기 좋아요
-            </span>
-            <Spacing size={2} />
-            <div
+            />
+          }
+          ref={appbarRef}
+        >
+          <img
+            src={data?.templateImageUrl ?? ""}
+            css={css`
+              width: 20rem;
+              height: auto;
+              position: absolute;
+              top: 50%;
+              left: 50%;
+              transform: translate(-50%, -50%);
+            `}
+          />
+        </TemplateLayout.Header>
+        <TemplateLayout.Main>
+          <section
+            css={css`
+              padding-top: 2rem;
+              display: flex;
+              flex-direction: column;
+              row-gap: 6rem;
+            `}
+            ref={contentRef}
+          >
+            <article
               css={css`
                 display: flex;
                 flex-direction: column;
-                row-gap: 0.8rem;
+                row-gap: 1.2rem;
               `}
             >
-              {/* FIXME: 데이터가 반복으로 들어가는 구간, 데이터 패칭 시 수정 */}
-              <QuestionBox
-                index={1}
-                title={"자유롭게 회고를 작성해주세요"}
-                contents={
-                  "앞서 말했던 내용의 해결책에 대해서 작성해요. 이때, 다음 회고 때 판별이 가능하거나 당장 실행 가능한 것들을 고려해서 작성하는 것이 좋아요.앞서 말했던 내용의 해결책에 대해서 작성해요. 이때, 다음 회고 때 판별이 가능하거나 당장 실행 가능한 것들을 고려해서 작성하는 것이 좋아요.앞서 말했던 내용의 해결책에 대해서 작성해요. 이때, 다음 회고 때 판별이 가능하거나 당장 실행 가능한 것들을 고려해서 작성하는 것이 좋아요.앞서 말했던 내용의 해결책에 대해서 작성해요. 이때, 다음 회고 때 판별이 가능하거나 당장 실행 가능한 것들을 고려해서 작성하는 것이 좋아요.앞서 말했던 내용의 해결책에 대해서 작성해요. 이때, 다음 회고 때 판별이 가능하거나 당장 실행 가능한 것들을 고려해서 작성하는 것이 좋아요.앞서 말했던 내용의 해결책에 대해서 작성해요. 이때, 다음 회고 때 판별이 가능하거나 당장 실행 가능한 것들을 고려해서 작성하는 것이 좋아요.앞서 말했던 내용의 해결책에 대해서 작성해요. 이때, 다음 회고 때 판별이 가능하거나 당장 실행 가능한 것들을 고려해서 작성하는 것이 좋아요.앞서 말했던 내용의 해결책에 대해서 작성해요. 이때, 다음 회고 때 판별이 가능하거나 당장 실행 가능한 것들을 고려해서 작성하는 것이 좋아요."
-                }
-              />
-              <QuestionBox
-                index={2}
-                title={"자유롭게 회고를 작성해주세요"}
-                contents={
-                  "앞서 말했던 내용의 해결책에 대해서 작성해요. 이때, 다음 회고 때 판별이 가능하거나 당장 실행 가능한 것들을 고려해서 작성하는 것이 좋아요.앞서 말했던 내용의 해결책에 대해서 작성해요. 이때, 다음 회고 때 판별이 가능하거나 당장 실행 가능한 것들을 고려해서 작성하는 것이 좋아요.앞서 말했던 내용의 해결책에 대해서 작성해요. 이때, 다음 회고 때 판별이 가능하거나 당장 실행 가능한 것들을 고려해서 작성하는 것이 좋아요.앞서 말했던 내용의 해결책에 대해서 작성해요. 이때, 다음 회고 때 판별이 가능하거나 당장 실행 가능한 것들을 고려해서 작성하는 것이 좋아요.앞서 말했던 내용의 해결책에 대해서 작성해요. 이때, 다음 회고 때 판별이 가능하거나 당장 실행 가능한 것들을 고려해서 작성하는 것이 좋아요.앞서 말했던 내용의 해결책에 대해서 작성해요. 이때, 다음 회고 때 판별이 가능하거나 당장 실행 가능한 것들을 고려해서 작성하는 것이 좋아요.앞서 말했던 내용의 해결책에 대해서 작성해요. 이때, 다음 회고 때 판별이 가능하거나 당장 실행 가능한 것들을 고려해서 작성하는 것이 좋아요.앞서 말했던 내용의 해결책에 대해서 작성해요. 이때, 다음 회고 때 판별이 가능하거나 당장 실행 가능한 것들을 고려해서 작성하는 것이 좋아요."
-                }
-              />
-              <QuestionBox
-                index={3}
-                title={"자유롭게 회고를 작성해주세요"}
-                contents={
-                  "앞서 말했던 내용의 해결책에 대해서 작성해요. 이때, 다음 회고 때 판별이 가능하거나 당장 실행 가능한 것들을 고려해서 작성하는 것이 좋아요.앞서 말했던 내용의 해결책에 대해서 작성해요. 이때, 다음 회고 때 판별이 가능하거나 당장 실행 가능한 것들을 고려해서 작성하는 것이 좋아요.앞서 말했던 내용의 해결책에 대해서 작성해요. 이때, 다음 회고 때 판별이 가능하거나 당장 실행 가능한 것들을 고려해서 작성하는 것이 좋아요.앞서 말했던 내용의 해결책에 대해서 작성해요. 이때, 다음 회고 때 판별이 가능하거나 당장 실행 가능한 것들을 고려해서 작성하는 것이 좋아요.앞서 말했던 내용의 해결책에 대해서 작성해요. 이때, 다음 회고 때 판별이 가능하거나 당장 실행 가능한 것들을 고려해서 작성하는 것이 좋아요.앞서 말했던 내용의 해결책에 대해서 작성해요. 이때, 다음 회고 때 판별이 가능하거나 당장 실행 가능한 것들을 고려해서 작성하는 것이 좋아요.앞서 말했던 내용의 해결책에 대해서 작성해요. 이때, 다음 회고 때 판별이 가능하거나 당장 실행 가능한 것들을 고려해서 작성하는 것이 좋아요.앞서 말했던 내용의 해결책에 대해서 작성해요. 이때, 다음 회고 때 판별이 가능하거나 당장 실행 가능한 것들을 고려해서 작성하는 것이 좋아요."
-                }
-              />
-            </div>
-          </article>
-          <article
-            css={css`
-              display: flex;
-              flex-direction: column;
-              row-gap: 1.2rem;
-            `}
-          >
-            <span
+              <span
+                id="title"
+                css={css`
+                  font-size: 2.4rem;
+                  font-weight: 600;
+                `}
+              >
+                {data?.title ?? ""}
+              </span>
+              <div
+                css={css`
+                  div {
+                    width: fit-content;
+                  }
+                `}
+              >
+                <TagBox tagName={data?.templateName ?? ""} />
+              </div>
+              <div
+                id="description"
+                css={css`
+                  color: #454952;
+                  line-height: 1.5;
+                `}
+              >
+                {data?.introduction ?? ""}
+              </div>
+            </article>
+            <article>
+              <span
+                css={css`
+                  color: #212329;
+                  font-weight: 600;
+                  font-size: 1.8rem;
+                `}
+              >
+                이런 목적으로 사용하기 좋아요
+              </span>
+              <Spacing size={2} />
+              <div
+                css={css`
+                  display: flex;
+                  align-items: center;
+                  column-gap: 0.8rem;
+                  row-gap: 0.8rem;
+                  flex-wrap: wrap;
+                `}
+              >
+                {/* FIXME: 서버에서 해당 데이터 값 반영 후, 리패칭 예정 */}
+                <PurposeBox purpose={"팀워크 강화"} />
+                <PurposeBox purpose={"팀워크 강화"} />
+                <PurposeBox purpose={"김현도리 김현도리 김현도리"} />
+                <PurposeBox purpose={"팀워크 강화"} />
+                <PurposeBox purpose={"김현도리 김현도리 김현도리"} />
+              </div>
+            </article>
+            <article>
+              <span
+                css={css`
+                  color: #212329;
+                  font-weight: 600;
+                  font-size: 1.8rem;
+                `}
+              >
+                이런 목적으로 사용하기 좋아요
+              </span>
+              <Spacing size={2} />
+              <div
+                css={css`
+                  display: flex;
+                  flex-direction: column;
+                  row-gap: 0.8rem;
+                `}
+              >
+                {/* FIXME: 데이터가 반복으로 들어가는 구간, 데이터 패칭 시 수정 */}
+                {data?.templateDetailQuestionList.map((item, index) => {
+                  return <QuestionBox key={item.questionId} index={index + 1} title={item.question} contents={item.description} />;
+                })}
+              </div>
+            </article>
+            <article
               css={css`
-                color: #212329;
-                font-weight: 600;
-                font-size: 1.8rem;
+                display: flex;
+                flex-direction: column;
+                row-gap: 1.2rem;
               `}
             >
-              회고 작성 Tip
-            </span>
-            <TipBox
-              title={"답변은 구체적으로!"}
-              content={
-                "MAD SAD GLAD 회고는 모든 작업 뒤에는 감정이 있다는 것을 상기하며 작업 지향적 회고에서 간과되는 근본적인 문제들을 드러낼 수 있게해요. 따라서 단순 감정 나열 보다는 왜 그런 감정을 느끼게 되었는지 구체적으로 작성하는게 중요해요. 사건의 배경을 설명하고 어떤 영향을 받았는지, 어떤 감정이 들었고 추후 생각하는 개선 방향이 있다면 제안하는 것도 큰 도움이 되어요. "
-              }
-            />
-            <ExampleButton> MAD SAD GLAD 회고 예시 보기 </ExampleButton>
-          </article>
-        </section>
-        <ButtonProvider>
-          <Button>선택하기</Button>
-        </ButtonProvider>
-      </TemplateLayout.Main>
-    </TemplateLayout>
+              <span
+                css={css`
+                  color: #212329;
+                  font-weight: 600;
+                  font-size: 1.8rem;
+                `}
+              >
+                회고 작성 Tip
+              </span>
+              <TipBox title={data?.tipTitle ?? ""} content={data?.tipDescription ?? ""} />
+              <ExampleButton> {data?.templateName ?? ""} 회고 예시 보기 </ExampleButton>
+            </article>
+          </section>
+          <ButtonProvider>
+            <Button>선택하기</Button>
+          </ButtonProvider>
+        </TemplateLayout.Main>
+      </TemplateLayout>
+    </Fragment>
   );
 }
