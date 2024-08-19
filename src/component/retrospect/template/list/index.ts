27dbf2d8906d4fcd8bb5886a9f3aeac11eb138b4
이민희 <mhl98@dgu.ac.kr>
2024-08-20 03:05:56 +0900

    fix: #151 회고 생성 템플릿 리스트의 경우 옵션 버튼 제거

--- src/component/retrospect/template/list/index.ts
+++ src/component/retrospect/template/list/index.ts
@@ -1,2 +1,3 @@
 export { CustomTemplateList } from "./CustomTemplateList";
 export { DefaultTemplateListItem } from "./DefaultTemplateListItem";
+export { CustomTemplateListItem } from "./CustomTemplateListItem";
