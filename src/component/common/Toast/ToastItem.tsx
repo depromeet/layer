7db43c17c0716f845182c68f2f48e1b067d875e0
donghunee <ehdgns1766@naver.com>
2024-08-19 18:46:22 +0900

    refactor: #35 toast 로직 수정

--- src/component/common/Toast/ToastItem.tsx
+++ src/component/common/Toast/ToastItem.tsx
@@ -2,9 +2,12 @@ import { css } from "@emotion/react";
 import { useRef, useState } from "react";
 
 import { Icon } from "@/component/common/Icon";
+import { Typography } from "@/component/common/typography";
 import { useToast } from "@/hooks/useToast";
 import { ANIMATION } from "@/style/common/animation";
 import { toastMap } from "@/style/common/toast";
+import { DESIGN_TOKEN_COLOR } from "@/style/designTokens";
+import { DESIGN_SYSTEM_COLOR } from "@/style/variable";
 import { ToastType } from "@/types/toast";
 import { collapseToast } from "@/utils/toast/collapseToast";
 
@@ -15,7 +18,6 @@ export function ToastItem({ type, content, id, duration = 3000 }: ToastType) {
   const [isPaused, setIsPaused] = useState(false);
 
   const handleExitingAnimationEnd = () => {
-    setIsClosing(true);
     collapseToast(toastRef.current!, () => {
       removeToast(id);
     });
@@ -40,9 +42,9 @@ export function ToastItem({ type, content, id, duration = 3000 }: ToastType) {
   const getIcon = () => {
     switch (type) {
       case "success":
-        return <Icon icon="ic_success" size={2.4} color="#A5C4F2" style={{ marginRight: "2rem" }} />;
+        return <Icon icon="ic_success" size={2.4} color={DESIGN_TOKEN_COLOR.blue600} style={{ marginRight: "0.8rem" }} />;
       case "error":
-        return <Icon icon="ic_success" size={2.4} color="#ffffff" style={{ marginRight: "2rem" }} />;
+        return <Icon icon="ic_warning" size={2.4} color={DESIGN_SYSTEM_COLOR.red500} style={{ marginRight: "0.8rem" }} />;
     }
   };
 
@@ -54,19 +56,21 @@ export function ToastItem({ type, content, id, duration = 3000 }: ToastType) {
         onAnimationEnd={handleExitingAnimationEnd}
         onClick={handleClick}
         css={css`
-          animation: 0.7s forwards ${isClosing ? ANIMATION.FADE_OUT : "none"};
+          animation: 0.7s forwards ${isClosing ? ANIMATION.FADE_OUT : ANIMATION.FADE_IN};
           width: 33.5rem;
-          padding: 2rem 2rem;
+          padding: 0rem 2rem;
+          height: 5.8rem;
           border-radius: 1.2rem;
           margin-bottom: 1rem;
           display: flex;
           align-items: center;
-
           ${toastMap[type]}
         `}
       >
         {getIcon()}
-        {content}
+        <Typography variant="B1" color="white">
+          {content}
+        </Typography>
         <div
           onAnimationEnd={handleTriggerAnimationEnd}
           css={css`
--- src/style/common/toast.ts
+++ src/style/common/toast.ts
@@ -2,11 +2,11 @@ import { css } from "@emotion/react";
 
 export const toastMap = {
   success: css`
-    background-color: rgba(33, 37, 41, 0.8);
+    background-color: rgba(6, 8, 12, 0.8);
     color: #ffffff;
   `,
   error: css`
-    background-color: rgba(225, 74, 74, 0.8);
+    background-color: rgba(6, 8, 12, 0.8);
     color: #ffffff;
   `,
 };
