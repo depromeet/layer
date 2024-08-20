4e38058554856ffc92f37dde7c573e17b031bd8c
이민희 <mhl98@dgu.ac.kr>
2024-08-20 23:28:11 +0900

    fix: #118 count view 레이아웃 수정

--- src/component/space/view/SpaceCountView.tsx
+++ src/component/space/view/SpaceCountView.tsx
@@ -19,64 +19,88 @@ export function SpaceCountView({ mainTemplate, memberCount }: SpaceCountViewProp
         background-color: ${DESIGN_TOKEN_COLOR.gray00};
         border: 1px solid rgba(33, 37, 41, 0.08);
         border-radius: 1.2rem;
-        padding: 1.6rem 2.8rem;
+        padding: 1.6rem 2.4rem;
         display: flex;
         justify-content: space-between;
         align-items: center;
       `}
     >
-      <div
+      <button
         css={css`
           display: flex;
           gap: 1.2rem;
           align-items: center;
+          justify-content: space-between;
+          flex-basis: 50%;
         `}
       >
-        <Icon icon="ic_template" size={2.8} color="blue600" />
         <div
           css={css`
             display: flex;
-            flex-direction: column;
-            gap: 0.2rem;
+            gap: 1rem;
           `}
         >
-          <Typography variant="body12Medium" color="gray800">
-            대표 템플릿
-          </Typography>
-          <Typography variant="subtitle14SemiBold">{mainTemplate ? categoryTagKoreanChange(mainTemplate) : "_"}</Typography>
+          <Icon icon="ic_template" size={2.8} color="blue600" />
+          <div
+            css={css`
+              display: flex;
+              flex-direction: column;
+              gap: 0.2rem;
+            `}
+          >
+            <Typography variant="body12Medium" color="gray800">
+              대표 템플릿
+            </Typography>
+            <Typography
+              variant="subtitle14SemiBold"
+              css={css`
+                text-align: left;
+              `}
+            >
+              {mainTemplate ? categoryTagKoreanChange(mainTemplate) : "-"}
+            </Typography>
+          </div>
         </div>
-      </div>
-
+        <Icon icon="ic_after" size={1.6} color={DESIGN_TOKEN_COLOR.gray400} />
+      </button>
       <div
         css={css`
           width: 1px;
           height: 2.6rem;
           background-color: #dee2e6;
+          margin: 0 2rem;
         `}
       />
-      <div
+      <button
         css={css`
           display: flex;
           align-items: center;
+          justify-content: space-between;
+          flex-basis: 50%;
         `}
       >
-        <Icon icon="ic_twoMan" size={2.8} />
         <div
           css={css`
             display: flex;
-            flex-direction: column;
-            gap: 0.2rem;
-            margin-left: 1.5rem;
-            margin-right: 1rem;
+            gap: 1rem;
           `}
         >
-          <Typography variant="body12Medium" color="gray800">
-            팀원
-          </Typography>
-          <Typography variant="subtitle14SemiBold">{memberCount}명</Typography>
+          <Icon icon="ic_twoMan" size={2.8} />
+          <div
+            css={css`
+              display: flex;
+              flex-direction: column;
+              gap: 0.2rem;
+            `}
+          >
+            <Typography variant="body12Medium" color="gray800">
+              팀원
+            </Typography>
+            <Typography variant="subtitle14SemiBold">{memberCount}명</Typography>
+          </div>
         </div>
-        <Icon icon="ic_after" size={1.6} />
-      </div>
+        <Icon icon="ic_after" size={1.6} color={DESIGN_TOKEN_COLOR.gray400} />
+      </button>
     </div>
   );
 }
