29fb878eb0b43fa0b1ffff2a5c562dce55620cdc
sean <sean2337@naver.com>
2024-08-20 21:58:33 +0900

    feat:#156 회고 분석 라우팅 설정

---
+++ src/app/retrospect/analysis/RetrospectAnalysisPage.tsx
@@ -0,0 +1,9 @@
+import { DualToneLayout } from "@/layout/DualToneLayout";
+
+export const RetrospectAnalysisPage = () => {
+  return (
+    <DualToneLayout bottomTheme="gray" title={"중간 발표 회고"}>
+      ss
+    </DualToneLayout>
+  );
+};
