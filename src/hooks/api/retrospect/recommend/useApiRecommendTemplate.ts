740549d755c843e45f5b688a5ae1534237398bc8
donghunee <ehdgns1766@naver.com>
2024-08-20 22:33:25 +0900

    refactor: #35 response type 추가

--- src/hooks/api/retrospect/recommend/useApiRecommendTemplate.ts
+++ src/hooks/api/retrospect/recommend/useApiRecommendTemplate.ts
@@ -3,15 +3,17 @@ import { useQuery } from "@tanstack/react-query";
 import { api } from "@/api";
 import { RecommendTemplateType } from "@/types/retrospectCreate/recommend";
 
+type RecommendTemplateResponse = {
+  formId: number;
+  formImageUrl: string;
+  formName: string;
+  tag: string;
+};
+
 export const useApiRecommendTemplate = (recommendValue: RecommendTemplateType) => {
   const recommendTemplate = async (recommendValue: RecommendTemplateType) => {
     const { period, periodic, purpose } = recommendValue;
-    const res = await api.get<{
-      formId: number;
-      formImageUrl: string;
-      formName: string;
-      tag: string;
-    }>(`/form/recommend`, {
+    const res = await api.get<RecommendTemplateResponse>(`/form/recommend`, {
       params: {
         periodic,
         period,
