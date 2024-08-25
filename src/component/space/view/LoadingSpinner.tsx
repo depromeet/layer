2ae085b15a83ea8126b405b75fb917c918125527
klmhyeonwoo <gentlemonster77@likelion.org>
2024-08-26 01:03:16 +0900

    feat: #35 Add Main Page Style

--- src/component/space/view/LoadingSpinner.tsx
+++ src/component/space/view/LoadingSpinner.tsx
@@ -1,34 +1,20 @@
 import { css } from "@emotion/react";
+import Lottie from "lottie-react";
+
+import loading from "@/assets/lottie/retropsect/list/loading.json";
 
 export function LoadingSpinner() {
   return (
-    <div
+    <Lottie
+      animationData={loading}
       css={css`
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        padding: 1rem;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 20rem;
+        height: auto;
       `}
-    >
-      <div
-        css={css`
-          border: 4px solid rgba(0, 0, 0, 0.1);
-          border-left-color: #09f;
-          border-radius: 50%;
-          width: 3rem;
-          height: 3rem;
-          animation: spin 1s linear infinite;
-
-          @keyframes spin {
-            0% {
-              transform: rotate(0deg);
-            }
-            100% {
-              transform: rotate(360deg);
-            }
-          }
-        `}
-      ></div>
-    </div>
+    />
   );
 }
