4ce34a1a342842312c9bf812b61cbbe19ed02db8
klmhyeonwoo <gentlemonster77@likelion.org>
2024-08-28 01:59:39 +0900

    feat: #83 Add Dynamic Open Graph

--- server/server.cjs
+++ server/server.cjs
@@ -7,6 +7,7 @@ const axios = require("axios");
 
 const app = express();
 
+// 프로젝트 루트 경로에서 dist 폴더 제공
 const distPath = path.resolve(__dirname, "../dist"); // 절대 경로 사용
 
 app.use(express.static(distPath));
@@ -16,7 +17,9 @@ app.get("/space/join/:id", async (req, res) => {
   const filePath = path.join(distPath, "index.html"); // dist 경로에 있는 index.html 사용
   let html;
 
+  // 1. HTML 파일 읽기
   try {
+    console.log(`Reading HTML file from ${filePath}`);
     html = fs.readFileSync(filePath, "utf8");
   } catch (err) {
     console.error("Failed to read index.html:", err);
@@ -24,9 +27,13 @@ app.get("/space/join/:id", async (req, res) => {
   }
 
   let leaderName, teamName;
+  console.log("VITE_API_URL:", process.env.VITE_API_URL);
+  console.log(`Decoded ID: ${atob(id)}`);
+
   try {
     const apiResponse = await axios.get(`${process.env.VITE_API_URL}/api/space/public/${atob(id)}`);
     const spaceData = apiResponse.data;
+    console.log("Space Data:", spaceData);
 
     leaderName = spaceData?.leader?.name;
     teamName = spaceData?.name;
