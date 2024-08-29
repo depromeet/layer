4e90aca20e3366b266472886b54b5247cb60014a
donghunee <ehdgns1766@naver.com>
2024-08-29 23:47:45 +0900

    fix: #234 암호화 키 생성

--- src/utils/space/cryptoKey.ts
+++ src/utils/space/cryptoKey.ts
@@ -6,17 +6,17 @@ const VECTOR_KEY = import.meta.env.VITE_VECTOR_KEY as string;
 const key = CryptoJS.enc.Utf8.parse(CRYPTO_KEY);
 const iv = CryptoJS.enc.Utf8.parse(VECTOR_KEY);
 
-function encryptId(id: string): string {
-  const encrypted = CryptoJS.AES.encrypt(id, key, {
+const encryptId = (id: string) => {
+  const encrypted = CryptoJS.AES.encrypt(id.toString(), key, {
     iv: iv,
     mode: CryptoJS.mode.CBC,
     padding: CryptoJS.pad.Pkcs7,
   });
 
   return encrypted.toString();
-}
+};
 
-function decryptId(encryptedId: string): string {
+const decryptId = (encryptedId: string) => {
   const decrypted = CryptoJS.AES.decrypt(encryptedId, key, {
     iv: iv,
     mode: CryptoJS.mode.CBC,
@@ -24,6 +24,6 @@ function decryptId(encryptedId: string): string {
   });
 
   return decrypted.toString(CryptoJS.enc.Utf8);
-}
+};
 
 export { encryptId, decryptId };
