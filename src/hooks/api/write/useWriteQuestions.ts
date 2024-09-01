533b1092bdbed4474ef1bcb58a88f66576ad4863
klm hyeon woo <gentlemonster77@likelion.org>
2024-09-01 15:50:45 +0900

    Update src/hooks/api/write/useWriteQuestions.ts

--- src/hooks/api/write/useWriteQuestions.ts
+++ src/hooks/api/write/useWriteQuestions.ts
@@ -18,7 +18,6 @@ export const useWriteQuestions = () => {
     /**
      * NOTE: 분석을 위한 Range 값을 실제 퍼센트로 변환하는 작업을 진행합니다.
      * index 형태를 통해 게이지를 채우기 때문에 index는 0으로 시작하는 형태를 지니고 있습니다,
-     * 그렇기 때문에 퍼센테이저로 변환하는 수식을 (index + 1) * 20로 설정을 합니다.
      *
      * Achievement Percent
      * - 0 : 20
