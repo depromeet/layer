6d9e9208d577a90976adaca1066f347bdc860e86
이민희 <mhl98@dgu.ac.kr>
2024-09-06 00:22:46 +0900

    chore: delete unused file

--- src/lib/mixpanel/event.ts
+++
@@ -1,9 +0,0 @@
-import mixpanel from "mixpanel-browser";
-
-export const trackEvent = (event: string, properties?: Record<string, string>) => {
-  if (import.meta.env.MODE === "development") {
-    console.log("mixpanel tracking:", event, properties);
-    return;
-  }
-  mixpanel.track(event, properties);
-};
