b4cf4a22ad88e1a41cf7b08821dbdd952b7a9533
klmhyeonwoo <gentlemonster77@likelion.org>
2024-11-03 21:04:09 +0900

    fix: #409 웹 환경에서 카카오 브라우저 로그인 시, 소셜 로그인 정책 우회

---
+++ apps/web/src/helper/preventExternalBrowser.tsx
@@ -0,0 +1,19 @@
+import { Fragment, PropsWithChildren } from "react";
+
+export function PreventExternalBrowser({ children }: PropsWithChildren) {
+  const agent = navigator.userAgent;
+  const URL = document.URL;
+  const isKakao = agent.includes("KAKAO");
+  const isInstagram = agent.includes("Instagram");
+
+  if (isKakao) {
+    return <span> 시스템 브라우저를 이용해주세요 </span>;
+    window.open(`kakaotalk://web/openExternal?url=${encodeURIComponent(URL)}`);
+  } else if (isInstagram) {
+    /**
+     * NOTE: 현재는 해당 인스타그램 인앱 탈출 코드가 작동하지 않는 것 같음
+     * 추후 카카오톡처럼 인스타그램 인앱 방지 분석 후 코드 추가 예정
+     */
+  }
+  return <Fragment> {children} </Fragment>;
+}
