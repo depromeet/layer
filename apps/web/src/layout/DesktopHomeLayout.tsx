1023b4bbef0ddc4eeab13b860230ec001733782b
prgmr99 <johnyeom24@naver.com>
2025-08-12 22:36:22 +0900

    chore: directory original path fixed

--- apps/web/src/layout/DesktopHomeLayout.tsx
+++ apps/web/src/layout/DesktopHomeLayout.tsx
@@ -1,7 +1,7 @@
 import { css } from "@emotion/react";
 import { Outlet } from "react-router-dom";
 
-import LocalNavigationBar from "@/component/common/LocalNavigationBar_backup";
+import LocalNavigationBar from "@/component/common/LocalNavigationBar";
 
 export default function DesktopHomeLayout() {
   return (
