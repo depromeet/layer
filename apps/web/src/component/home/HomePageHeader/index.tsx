64dc4edf5bdaff62f52509eaa1108a4f9c172cbc
prgmr99 <johnyeom24@naver.com>
2025-09-11 17:17:36 +0900

    remove: #496 컴포넌트 제거

--- apps/web/src/component/home/HomePageHeader/index.tsx
+++ apps/web/src/component/home/HomePageHeader/index.tsx
@@ -1,6 +1,5 @@
 import { Typography } from "@/component/common/typography";
 import { css } from "@emotion/react";
-import QuickActionButton from "../QuickActionButton";
 
 export default function HomePageHeader() {
   return (
@@ -14,20 +13,6 @@ export default function HomePageHeader() {
       <Typography color="gray900" variant="heading28Bold">
         내 회고 리포트
       </Typography>
-
-      <section
-        css={css`
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          gap: 0.8rem;
-          margin-top: 1.5rem;
-        `}
-      >
-        <QuickActionButton action="작성 예정 회고보기" />
-        <QuickActionButton action="회고 생성하기" />
-        <QuickActionButton action="회고 작성하기" />
-      </section>
     </header>
   );
 }
