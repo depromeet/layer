8cfc93622f3c668ec5beff309ef2790f0370290e
klmhyeonwoo <gentlemonster77@likelion.org>
2025-09-17 21:26:11 +0900

    feat: #497 useDeviceType 관련 부가 값 추가

--- apps/web/src/hooks/useDeviceType.ts
+++ apps/web/src/hooks/useDeviceType.ts
@@ -1,11 +1,20 @@
 import { useEffect, useState } from "react";
 
-export function useDeviceType(threshold = 768): { deviceType: "mobile" | "desktop" } {
+interface useDeviceReturnType {
+  deviceType: "mobile" | "desktop";
+  isDesktop: boolean;
+  isMobile: boolean;
+}
+
+export function useDeviceType(threshold = 768): useDeviceReturnType {
   const [deviceType, setDeviceType] = useState<"mobile" | "desktop">("desktop");
+  const { pathname } = window.location;
+  const isDesktop = deviceType === "desktop" && !pathname.startsWith("/m");
+  const isMobile = deviceType === "mobile" || pathname.startsWith("/m");
 
   useEffect(() => {
     setDeviceType(window.innerWidth <= threshold ? "mobile" : "desktop");
   }, []);
 
-  return { deviceType };
+  return { deviceType, isDesktop, isMobile };
 }
