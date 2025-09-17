ff82366cca0957aee21b49064a2e288785d33b45
JaeIn1 <ysy06053@gmail.com>
2025-09-17 23:47:06 +0900

    fix: Explanation, Title 타입 조건문 추가

--- apps/web/src/component/common/header/HeaderProvider.tsx
+++ apps/web/src/component/common/header/HeaderProvider.tsx
@@ -8,7 +8,7 @@ import { TypographyProps } from "@/component/common/typography/Typography.tsx";
 export type ThemeSet = "white" | "primary";
 export type HeaderProps = {
   theme?: ThemeSet;
-  contents: string;
+  contents: ReactNode | string;
   type?: string;
 } & Omit<React.HTMLAttributes<HTMLDivElement>, "type"> &
   Omit<TypographyProps, "children">;
