6c56e0ec190272f69ea7d9b00297f7a81765cf44
정현우 <nolang3748@naver.com>
2025-09-20 21:54:09 +0900

    fix: 내스페이스 min-width 적용

--- apps/web/src/app/desktop/retrospectSpace/RetroSpectSpacePage.tsx
+++ apps/web/src/app/desktop/retrospectSpace/RetroSpectSpacePage.tsx
@@ -14,6 +14,7 @@ export default function RetroSpectSpacePage() {
         display: flex;
         flex-direction: column;
         gap: 1.8rem;
+        min-width: 92.8rem;
       `}
     >
       <RetrospectSpaceHeader />
