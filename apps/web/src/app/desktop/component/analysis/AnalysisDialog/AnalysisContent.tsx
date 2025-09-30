e58032ca781579fee4925ee9c89f96c429d1df43
prgmr99 <johnyeom24@naver.com>
2025-09-30 21:15:30 +0900

    feat: #525 회고 분석 다이얼로그 - 질문(목표했던 부분에 얼마나 달성했나요?) UI 적용

--- apps/web/src/app/desktop/component/analysis/AnalysisDialog/AnalysisContent.tsx
+++ apps/web/src/app/desktop/component/analysis/AnalysisDialog/AnalysisContent.tsx
@@ -1,11 +1,16 @@
 import { Typography } from "@/component/common/typography";
-import { CSatisfactionTemplate } from "@/component/write/template/complete";
+import { CAchievementTemplate, CSatisfactionTemplate } from "@/component/write/template/complete";
 import { DESIGN_TOKEN_COLOR } from "@/style/designTokens";
 import { css } from "@emotion/react";
 
 export default function AnalysisContent() {
   return (
-    <section>
+    <section
+      css={css`
+        display: flex;
+        gap: 2rem;
+      `}
+    >
       <article
         css={css`
           width: 34rem;
@@ -26,6 +31,29 @@ export default function AnalysisContent() {
           <CSatisfactionTemplate name="송채현" index={4} />
           <CSatisfactionTemplate name="송채현" index={4} />
           <CSatisfactionTemplate name="송채현" index={4} />
+        </article>
+      </article>
+
+      <article
+        css={css`
+          width: 34rem;
+          background-color: ${DESIGN_TOKEN_COLOR.gray100};
+          border-radius: 1.2rem;
+          margin-top: 2rem;
+          padding: 2rem 1.6rem;
+        `}
+      >
+        <Typography variant="body15Normal" color="gray900">
+          목표했던 부분에 얼마나 달성했나요?
+        </Typography>
+
+        <article>
+          <CAchievementTemplate name="송채현" index={4} />
+          <CAchievementTemplate name="송채현" index={4} />
+          <CAchievementTemplate name="송채현" index={4} />
+          <CAchievementTemplate name="송채현" index={4} />
+          <CAchievementTemplate name="송채현" index={4} />
+          <CAchievementTemplate name="송채현" index={4} />
         </article>
       </article>
     </section>
