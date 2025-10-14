8cdad60f1d0b3a89359bb327e0b470fb8c49b4d0
정현우 <nolang3748@naver.com>
2025-10-14 23:11:04 +0900

    feat: desktop mobile view 구분 처리

--- apps/web/src/component/retrospect/template/list/CustomTemplateListItem.tsx
+++ apps/web/src/component/retrospect/template/list/CustomTemplateListItem.tsx
@@ -1,14 +1,16 @@
 import { css } from "@emotion/react";
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 import { TemplateListPageContext } from "@/app/desktop/component/retrospect/template/list";
 import { BottomSheet } from "@/component/BottomSheet";
-import { ButtonProvider } from "@/component/common/button";
+import { Button, ButtonProvider } from "@/component/common/button";
 import { Card } from "@/component/common/Card";
 import { DropdownMenu } from "@/component/common/dropdownMenu/DropdownMenu";
 import { TextArea } from "@/component/common/input";
 import { Tag } from "@/component/common/tag";
 import { Typography } from "@/component/common/typography";
+import { PATHS } from "@layer/shared";
 import { useDeleteCustomTemplate } from "@/hooks/api/template/useDeleteCustomTemplate";
 import { usePatchTemplateTitle } from "@/hooks/api/template/usePatchTemplateTitle";
 import { useBottomSheet } from "@/hooks/useBottomSheet";
@@ -16,6 +18,7 @@ import { useInput } from "@/hooks/useInput";
 import { useModal } from "@/hooks/useModal";
 import { useFunnelModal } from "@/hooks/useFunnelModal";
 import CustomTemplateListDetailItem from "@/app/desktop/component/retrospect/template/list/CustomTemplateListDetailItem";
+import { getDeviceType } from "@/utils/deviceUtils";
 
 type CustomTemplateListItem = {
   id: number;
@@ -26,17 +29,19 @@ type CustomTemplateListItem = {
 
 export function CustomTemplateListItem({ id, title, tag, date }: CustomTemplateListItem) {
   const { openFunnelModal } = useFunnelModal();
+  const navigate = useNavigate();
 
   const MENU_EDIT = "edit-name";
   const MENU_DELETE = "delete";
   const SHEET_ID = `modifyTemplateSheet_${id}`;
 
-  const { spaceId, readOnly } = useContext(TemplateListPageContext);
+  const { spaceId, readOnly, isLeader } = useContext(TemplateListPageContext);
   const { open } = useModal();
   const { openBottomSheet, closeBottomSheet } = useBottomSheet();
   const { value: templateTitle, handleInputChange: handleChangeTitle } = useInput(title);
   const { mutate: patchTemplateTitle } = usePatchTemplateTitle(+spaceId);
   const { mutate: deleteCustomTemplate } = useDeleteCustomTemplate(+spaceId);
+  const { isDesktop } = getDeviceType();
 
   const handleSubmitTitle = () => {
     patchTemplateTitle({ formId: id, formTitle: templateTitle });
@@ -88,7 +93,7 @@ export function CustomTemplateListItem({ id, title, tag, date }: CustomTemplateL
             css={css`
               display: flex;
               flex-direction: column;
-              gap: 4rem;
+              gap: ${isDesktop ? "4rem" : "1.2rem"};
             `}
           >
             <div
@@ -97,44 +102,68 @@ export function CustomTemplateListItem({ id, title, tag, date }: CustomTemplateL
                 justify-content: space-between;
               `}
             >
-              <Typography variant={"subtitle14SemiBold"}>{title}</Typography>
-              <DropdownMenu onValueChange={(value) => handleOptionSelect(value)}>
-                <DropdownMenu.Trigger />
-                <DropdownMenu.Content>
-                  <DropdownMenu.Item value={MENU_EDIT}>
-                    <Typography variant={"subtitle14SemiBold"}>이름 수정하기</Typography>
-                  </DropdownMenu.Item>
-                  <DropdownMenu.Item value={MENU_DELETE}>
-                    <Typography variant={"subtitle14SemiBold"} color={"red500"}>
-                      삭제하기
-                    </Typography>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu>
+              <Typography variant={isDesktop ? "subtitle14SemiBold" : "S2"}>{title}</Typography>
+              {readOnly && isLeader && (
+                <DropdownMenu onValueChange={(value) => handleOptionSelect(value)}>
+                  <DropdownMenu.Trigger />
+                  <DropdownMenu.Content>
+                    <DropdownMenu.Item value={MENU_EDIT}>
+                      <Typography variant={"subtitle14SemiBold"}>이름 수정하기</Typography>
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item value={MENU_DELETE}>
+                      <Typography variant={"subtitle14SemiBold"} color={"red500"}>
+                        삭제하기
+                      </Typography>
+                    </DropdownMenu.Item>
+                  </DropdownMenu.Content>
+                </DropdownMenu>
+              )}
             </div>
-            <div
-              css={css`
-                display: flex;
-                justify-content: space-between;
-                align-items: center;
-              `}
-            >
-              <Tag>{tag}</Tag>
+            {isDesktop ? (
               <div
-                css={
-                  readOnly &&
-                  css`
-                    align-self: flex-end;
-                    margin-top: -0.6rem;
-                  `
-                }
+                css={css`
+                  display: flex;
+                  justify-content: space-between;
+                  align-items: center;
+                `}
               >
+                <Tag>{tag}</Tag>
                 <Typography variant={"body14Medium"} color={"gray500"}>
                   {date}
                 </Typography>
               </div>
-            </div>
+            ) : (
+              <>
+                <Tag>{tag}</Tag>
+                <div
+                  css={
+                    readOnly &&
+                    css`
+                      align-self: flex-end;
+                      margin-top: -0.6rem;
+                    `
+                  }
+                >
+                  <Typography variant={"body14Medium"} color={"gray600"}>
+                    {date}
+                  </Typography>
+                </div>
+              </>
+            )}
           </div>
+          {!isDesktop && !readOnly && (
+            <Button
+              colorSchema={"outline"}
+              onClick={(e) => {
+                e.stopPropagation();
+                navigate(PATHS.retrospectCreate(), {
+                  state: { spaceId, templateId: id },
+                });
+              }}
+            >
+              선택하기
+            </Button>
+          )}
         </Card>
       </li>
       <BottomSheet
