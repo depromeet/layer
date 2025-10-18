8f6a94494a7357d182f4c747b73fc7a9e0994888
klmhyeonwoo <gentlemonster77@likelion.org>
2025-10-19 00:10:26 +0900

    feat: #566 스페이스 및 회고 생성 플로우 연동

--- apps/web/src/hooks/api/space/useApiPostSpace.ts
+++ apps/web/src/hooks/api/space/useApiPostSpace.ts
@@ -6,10 +6,12 @@ import { api } from "@/api";
 import { useMixpanel } from "@/lib/provider/mix-pannel-provider";
 import { spaceState } from "@/store/space/spaceAtom";
 import { SpaceValue } from "@/types/space";
+import { getDeviceType } from "@/utils/deviceUtils";
 
 export const useApiPostSpace = () => {
   const navigate = useNavigate();
   const resetSpaceValue = useResetAtom(spaceState);
+  const { isMobile } = getDeviceType();
   const { track } = useMixpanel();
 
   const postSpace = async (formData: SpaceValue) => {
@@ -33,9 +35,10 @@ export const useApiPostSpace = () => {
         introduction: variables.introduction,
         name: variables.name,
       });
-
-      navigate(PATHS.spaceCreateDone(), { state: { spaceId } });
-      resetSpaceValue();
+      if (isMobile) {
+        navigate(PATHS.spaceCreateDone(), { state: { spaceId } });
+        resetSpaceValue();
+      }
     },
     onError: (error) => {
       console.log(error);
