a5ef932ffbddd081da18d1b13c6a695d052266a6
klmhyeonwoo <gentlemonster77@likelion.org>
2025-10-22 00:24:03 +0900

    feat: 비효율적인 객체 구조에서 각 개별 아톰을 가지는 스토어 구조로 변경 #589

--- apps/web/src/store/retrospect/retrospectCreate.ts
+++ apps/web/src/store/retrospect/retrospectCreate.ts
@@ -2,6 +2,35 @@ import { atomWithReset } from "jotai/utils";
 
 import { RetrospectCreateReq } from "@/types/retrospectCreate";
 
+export const DEFAULT_QUESTIONS = [
+  {
+    questionContent: "진행 상황에 대해 얼마나 만족하나요?",
+    questionType: "number",
+  },
+  {
+    questionContent: "목표했던 부분에 얼마나 달성했나요?",
+    questionType: "range",
+  },
+] as RetrospectCreateReq["questions"];
+
+const CREATE_RETROSPECT_INIT_TITLE = atomWithReset<RetrospectCreateReq["title"]>("");
+const CREATE_RETROSPECT_INIT_INTRODUCTION = atomWithReset<RetrospectCreateReq["introduction"]>("");
+const CREATE_RETROSPECT_INIT_QUESTIONS = atomWithReset<RetrospectCreateReq["questions"]>([]);
+const CREATE_RETROSPECT_INIT_DEADLINE = atomWithReset<RetrospectCreateReq["deadline"]>("");
+const CREATE_RETROSPECT_INIT_IS_NEW_FORM = atomWithReset<RetrospectCreateReq["isNewForm"]>(false);
+const CREATE_RETROSPECT_INIT_HAS_CHANGED_ORIGINAL = atomWithReset<RetrospectCreateReq["hasChangedOriginal"]>(false);
+const CREATE_RETROSPECT_INIT_CUR_FORM_ID = atomWithReset<RetrospectCreateReq["curFormId"]>(-1);
+
+export const CREATE_RETROSPECT_INIT_ATOM = {
+  title: CREATE_RETROSPECT_INIT_TITLE,
+  introduction: CREATE_RETROSPECT_INIT_INTRODUCTION,
+  questions: CREATE_RETROSPECT_INIT_QUESTIONS,
+  deadline: CREATE_RETROSPECT_INIT_DEADLINE,
+  isNewForm: CREATE_RETROSPECT_INIT_IS_NEW_FORM,
+  hasChangedOriginal: CREATE_RETROSPECT_INIT_HAS_CHANGED_ORIGINAL,
+  curFormId: CREATE_RETROSPECT_INIT_CUR_FORM_ID,
+};
+
 export const retrospectCreateAtom = atomWithReset<RetrospectCreateReq>({
   title: "",
   introduction: "",
