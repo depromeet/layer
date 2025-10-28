700fbf41b0ef9505ae79015646548aa6ea05600d
klmhyeonwoo <gentlemonster77@likelion.org>
2025-10-28 21:15:28 +0900

    fix: 화면 가로 너비가 좁을 때, 버튼 텍스트가 내려가 화면 일부가 깨져보이는 현상 수정

--- apps/web/src/app/desktop/component/analysis/AnalysisOverview/AnalysisOverviewHeader.tsx
+++ apps/web/src/app/desktop/component/analysis/AnalysisOverview/AnalysisOverviewHeader.tsx
@@ -113,6 +113,7 @@ export default function AnalysisOverviewHeader() {
             justify-content: center;
             gap: 0.4rem;
             cursor: pointer;
+            white-space: nowrap;
           `}
           onClick={handleRetrospectCreate}
         >
