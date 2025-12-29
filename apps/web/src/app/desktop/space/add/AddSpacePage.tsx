dad12301f239e0ceb25f729e15d2e3dc0b777f02
JaeIn1 <ysy06053@gmail.com>
2025-12-29 20:54:57 +0900

    style: 스페이스 생서 - 회고 플로우 title, tag, 질문수정 버튼 레이아웃 수정

--- apps/web/src/app/desktop/space/add/AddSpacePage.tsx
+++ apps/web/src/app/desktop/space/add/AddSpacePage.tsx
@@ -751,15 +751,39 @@ function CreateRetrospectQuestionFunnel() {
           overflow-y: auto;
         `}
       >
-        <Typography
-          variant={"S1"}
+        <div
           css={css`
-            padding-right: 13rem;
+            display: flex;
+            justify-content: space-between;
+            align-items: start;
           `}
         >
-          {customTemplateInfo.title}
-        </Typography>
-        <Tag styles="margin-top: 0.8rem">{customTemplateInfo.tag}</Tag>
+          <div
+            css={css`
+              padding-top: 0.2rem;
+            `}
+          >
+            <Typography variant={"S1"}>{customTemplateInfo.title}</Typography>
+            <Tag styles="margin-top: 0.8rem">{customTemplateInfo.tag}</Tag>
+          </div>
+          <QuestionEditButton
+            onClose={() => {
+              openDesktopModal({
+                title: "",
+                contents: <AddSpacePage />,
+                options: {
+                  enableFooter: false,
+                  needsBackButton: false,
+                },
+                onClose: () => {
+                  resetRetrospectInfo();
+                  resetSpaceInfo();
+                },
+              });
+            }}
+          />
+        </div>
+
         <Spacing size={3} />
         <div
           css={css`
@@ -775,22 +799,6 @@ function CreateRetrospectQuestionFunnel() {
             ))}
           </QuestionList>
         </div>
-        <QuestionEditButton
-          onClose={() => {
-            openDesktopModal({
-              title: "",
-              contents: <AddSpacePage />,
-              options: {
-                enableFooter: false,
-                needsBackButton: false,
-              },
-              onClose: () => {
-                resetRetrospectInfo();
-                resetSpaceInfo();
-              },
-            });
-          }}
-        />
       </div>
       <Spacing size={4} />
       <ButtonProvider
