0d73c0266376c27dc893d27d762b75a49a00bda5
klmhyeonwoo <gentlemonster77@likelion.org>
2026-02-18 23:11:28 +0900

    feat: Add Clarity Analytics

---
+++ apps/web/src/lib/provider/clarity-provider/index.tsx
@@ -0,0 +1,13 @@
+import { createContext } from "@/lib/create-context";
+import { PropsWithChildren } from "react";
+import Clarity from "@microsoft/clarity";
+
+export default function index({ children }: PropsWithChildren) {
+  if (import.meta.env.MODE === "production") {
+    Clarity.init("vj5mt0s50u");
+  }
+
+  // TODO: 이벤트 분석 후에, 컨텍스트 API 이벤트 구체화
+  const [Provider] = createContext("CLARITY_PROVIDER");
+  return <Provider> {children} </Provider>;
+}
