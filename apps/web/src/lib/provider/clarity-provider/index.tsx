9598b650cac40d3ea9ead21e9d36b798f42752b8
klmhyeonwoo <gentlemonster77@likelion.org>
2026-02-18 23:15:13 +0900

    fix: env

--- apps/web/src/lib/provider/clarity-provider/index.tsx
+++ apps/web/src/lib/provider/clarity-provider/index.tsx
@@ -4,7 +4,7 @@ import Clarity from "@microsoft/clarity";
 
 export default function ClarityProvider({ children }: PropsWithChildren) {
   if (import.meta.env.MODE === "production") {
-    Clarity.init("vj5mt0s50u");
+    Clarity.init(import.meta.env.VITE_CLARITY_ANALYTICS);
   }
 
   // TODO: 이벤트 분석 후에, 컨텍스트 API 이벤트 구체화
