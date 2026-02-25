c9fa1081d9e0b4b4fe06a5999522cc434c5cda66
prgmr99 <johnyeom24@naver.com>
2026-02-25 14:46:43 +0900

    remove: #825 사용하지 않는 컴포넌트 제거

--- apps/web/src/component/home/index.ts
+++ apps/web/src/component/home/index.ts
@@ -1,4 +1,3 @@
-export { ViewSelectTab } from "./ViewSelectTab";
 export { GoMakeReviewButton } from "./GoMakeReviewButton";
 export { SpaceOverview } from "./SpaceOverview";
 export { NavigationBar } from "./NavigaionBar";
