c5dc76a353980c5fb896612e593dd64dd9538d43
klmhyeonwoo <gentlemonster77@likelion.org>
2026-03-31 00:56:17 +0900

    fix: 메인 온보딩 캐러셀의 위치를 중앙으로 이동

--- apps/web/src/app/desktop/login/DesktopLoginPage.tsx
+++ apps/web/src/app/desktop/login/DesktopLoginPage.tsx
@@ -68,7 +68,7 @@ function SwiperSlideOnboardingImage({
         flex-direction: column;
         justify-content: flex-start;
         align-items: center;
-        padding-top: 6.6rem;
+        justify-content: center;
       `}
     >
       <div
