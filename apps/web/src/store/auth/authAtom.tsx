601d76c69edbed27407e0ac64ac245964c29bb6b
klmhyeonwoo <gentlemonster77@likelion.org>
2026-04-14 19:54:01 +0900

    feat: 로그인 시에 할당되는 A/B 테스트의 사용자 그룹 소속 전역 데이터 설정

--- apps/web/src/store/auth/authAtom.tsx
+++ apps/web/src/store/auth/authAtom.tsx
@@ -1,3 +1,4 @@
+import { atom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 
 type Auth = {
@@ -16,4 +17,10 @@ export const authAtom = atomWithStorage<Auth>("auth", {
   memberRole: "",
   imageUrl: "",
   memberSeq: null,
+});
+
+// 로그인 시에 할당되는 A/B 테스트의 사용자 그룹 소속 데이터
+export const branchLayoutAtom = atom((get) => {
+  const { memberSeq } = get(authAtom);
+  return memberSeq ? (memberSeq % 2 === 0 ? "A" : "B") : "B";
 });
